62f49494f1733488266a21f3aad3fd8ca328ff4c
Colleen Bertoni <bertoni@anl.gov>
2018-03-18 18:31:46 +0000

    Distance class addition: Removing LibChemisDefs.h

--- LibChemist/Distance.hpp
+++ LibChemist/Distance.hpp
@@ -4,6 +4,17 @@
 
 namespace LibChemist {
 
+  ///These are the recognized types of IntCoords
+  namespace IntCoord_t {
+    constexpr auto BOND="BOND";///<A bond
+    constexpr auto PAIR13="PAIR13";///<A 1,3 pair
+    constexpr auto PAIR14="PAIR14";///<A 1,4 pair
+    constexpr auto PAIR="PAIR";///<A pair that is not a 1,2; 1,3; or 1,4 pair
+    constexpr auto ANGLE="ANGLE";///<An angle
+    constexpr auto TORSION="TORSION";///<A torsion
+    constexpr auto IMPTORSION="IMPTORSION";///<An improper torsion angle
+  }
+
   class Distance {
     const std::string name;
   public:
