d8dc50e3c1faf45a9afee236390dc9532c7a3f5d
ryanmrichard <ryanmrichard1@gmail.com>
2018-07-02 11:57:52 -0500

    hunter, now w/o submodules!!!!

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -1,4 +1,6 @@
 hunter_config(
-        Utilities GIT_SUBMODULE "external/Utilities"
-                  CMAKE_ARGS "BUILD_TESTS=off"
+        Utilities
+        VERSION 0.0.0
+        URL "https://api.github.com/repos/NWChemEx-Project/Utilities/tarball/master?access_token=d28e8df2f89944f49e7144c747ffe3a114547469"
+        SHA1 "42446742165e643a10778f4a3402a5164728d0bb"
 )
