a9d6d286fa7d2668a98071f68b538c505f9c2915
ryanmrichard <ryanmrichard1@gmail.com>
2018-07-02 13:43:13 -0500

    Forgot a headerfile

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -1,6 +1,9 @@
+set(gh_url "https://api.github.com/repos/NWChemEx-Project/Utilities")
+set(token "d28e8df2f89944f49e7144c747ffe3a114547469")
 hunter_config(
         Utilities
         VERSION 0.0.0
-        URL "https://api.github.com/repos/NWChemEx-Project/Utilities/tarball/master?access_token=d28e8df2f89944f49e7144c747ffe3a114547469"
+        URL "${gh_url}/tarball/master?access_token=${token}"
         SHA1 "42446742165e643a10778f4a3402a5164728d0bb"
+        CMAKE_ARGS "BUILD_TESTS=off"
 )
