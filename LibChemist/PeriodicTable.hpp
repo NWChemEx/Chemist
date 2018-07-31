cbb3cee1f8ee01b8d1a18619a173067632bd3040
ryanmrichard <ryanmrichard1@gmail.com>
2018-07-31 11:42:35 -0500

    PeriodicTable class done

--- LibChemist/PeriodicTable.hpp
+++ LibChemist/PeriodicTable.hpp
@@ -1,10 +1,13 @@
 #pragma once
 #include "LibChemist/Atom.hpp"
+#include <memory> //For unique_ptr
+#include <vector>
 
 namespace LibChemist {
 namespace detail_ {
+/// Forward declaration of the PIMPL class
 class PeriodicTablePIMPL;
-}
+} // namespace detail_
 
 /**
  * @brief A class serving as a factory for Atom instances
@@ -16,6 +19,45 @@ class PeriodicTable {
 public:
     /// The type of a counting number
     using size_type = std::size_t;
+
+    /// The type of a list of isotopes
+    using isotope_list = std::vector<size_type>;
+
+    /**
+     * @brief Constructs a PTable loaded with the NWChemEx defaults.
+     *
+     * Default values for an NWChemEx PTable instance are populated using the
+     * generate_atomicinfo.py script, which parses the accompanying data files
+     * taken from IUPAC's CIAWW division.
+     *
+     * @throw std::bad_alloc if there is insufficient memory to allocate the
+     *        PIMPL.  Strong throw guarantee.
+     */
+    PeriodicTable();
+
+    /**
+     * @defgroup Copy/Move CTors and Assignment Ops
+     * @brief Functions for setting the state of the current PeriodicTable
+     * instance off of another instance.
+     *
+     * All copies are deep copies.
+     *
+     * @param[in] rhs The instance to copy/move from.  For move operations @p
+     * rhs is in a valid, but otherwise undefined state.
+     *
+     * @throw std::bad_alloc if the copy ctor/assignment operator has
+     * insufficient memory to complete the operation.  Strong throw guarantee.
+     * All move operations are no throw guarantee.
+     */
+    ///@{
+    PeriodicTable(const PeriodicTable& rhs);
+    PeriodicTable(PeriodicTable&& rhs) noexcept;
+    PeriodicTable& operator=(const PeriodicTable& rhs);
+    PeriodicTable& operator=(PeriodicTable&& rhs) noexcept;
+    ///@}
+
+    /// Default dtor
+    ~PeriodicTable() noexcept;
 
     /**
      * @brief Returns the maximum atomic number for which an Atom instance can
@@ -33,28 +75,43 @@ public:
      *
      * @throw None no throw guarantee.
      */
-    constexpr size_type max_Z() const noexcept;
+    size_type max_Z() const noexcept;
+
+    /**
+     * @brief Returns a list of available mass numbers for a particular atom
+     *
+     * @param Z The atomic number of the atom, for which mass numbers are
+     *        wanted.  Should be in range [0, max_Z())
+     * @return A list of available mass numbers for the requested atom.  If no
+     *         isotope data is available for the particular value of @p Z then
+     *         the list is empty.
+     * @throw std::bad_alloc if there is insufficient memory to allocate the
+     *        list.  Strong throw guarantee.
+     * @throw std::out_of_range if @p Z is not in the range [0, max_Z()).
+     *        Strong throw guarantee.
+     */
+    isotope_list isotopes(size_type Z) const;
 
     /**
      * @defgroup Atom Creation Functions
      * @brief Returns an Atom instance pre-loaded with the data for the
      *        specified atomic number.
      *
-     * In chemistry we are used to thinking of an atom as being uniquely
-     * specified simply by the atomic number.  In truth, the atomic number
-     * specifies everything but the mass.  Unfortunately there are two masses
-     * one can reasonably use for an atom: the abundance weighted mass and the
-     * isotope mass.  We provide two different functions to distinguish between
-     * these use cases.  `get_atom` returns the abundance weighted variant,
-     * whereas `get_isotope` returns an Atom whose mass is for a particular
-     * isotope.
+     * There are two masses one can reasonably use for an atom: the abundance
+     * weighted mass and the isotope mass.  We provide two different functions
+     * to distinguish between these use cases.  `get_atom` returns the
+     * abundance weighted variant, whereas `get_isotope` returns an Atom
+     * whose mass is for a particular isotope.
      *
      * @param Z The atomic number of the atom to create.  Should be in the range
      *        [0, max_Z()).
      * @param mass_num The mass number of the isotope to create.
-     * @return The requested atom.
+     * @return The requested atom as new instance.
      * @throw std::bad_alloc if there is insufficient memory to make the Atom
      *        instance.  Strong throw guarantee.
+     * @throw std::out_of_range if @p Z is greater than max_Z() or if there is
+     *        no isotope for a particular @p Z @p mass_num pair.  Strong throw
+     *        guarantee.
      */
     ///@{
     Atom get_atom(size_type Z) const;
--- reference_data/generate_atomicinfo.py
+++ reference_data/generate_atomicinfo.py
@@ -1,6 +1,6 @@
 # #!/usr/bin/env python3
 import os
-from helper_fxns import *
+import re
 
 """This script is used to create the experimental data look up tables for the
 atom class.
@@ -18,161 +18,252 @@ script:
 
 """
 
-class IsotopeInfo:
-    """ Class for storing the isotope info"""
-    def __init__(self, iso_num, mass):
-        self.isonum = iso_num
-        self.mass = mass
-        self.abundance = 0.0
-
-    def cxxify(self, f_in):
-        """Prints the class out using aggregate initialization"""
-        f_in.write("IsotopeData{")  # Start class
-        f_in.write("{},".format(self.isonum))
-        f_in.write("{},".format(self.mass))
-        f_in.write("{}".format(self.abundance))
-        f_in.write("}")  # End class
-
-class AtomicInfo:
-    """ Class for storing the information relating to an atom
-
-    """
-    def __init__(self, z_in):
-        self.Z = int(z_in)
-        self.mult = 0
+class AtomicData:
+    def __init__(self):
+        self.sym = ""
+        self.name = ""
+        self.Z = 0
+        self.isotopes = []
         self.mass = 0.0
-        self.cov_radius = 0.0
-        self.vdw_radius = 0.0
-        self.isotopes = {}
+        self.isotope_masses = {}
 
-    def cxxify(self, f_in):
-        prop_add = "      {" \
-                   "LibChemist::Atom::Property::"
-        f_in.write("{}charge, {}}},\n".format(prop_add, self.Z))
-        f_in.write("{}mass, {}}},\n".format(prop_add,self.mass))
-        f_in.write("{}cov_radius, {}}},\n".format(prop_add, self.cov_radius))
-        f_in.write("{}vdw_radius, {}}}\n".format(prop_add, self.vdw_radius))
+    def add_isotope(self, num, mass):
+        self.isotope_masses[num] = mass
+        if not num in self.isotopes:
+            self.isotopes.append(num)
+
+    def __repr__(self):
+        rv = "{} {} {}[".format(self.Z, self.name, self.mass)
+        for x in self.isotopes:
+            rv +="{}: {}".format(x, self.isotope_masses[x])
+            if x!= self.isotopes[-1]:
+                rv+=", "
+        rv += "]"
+        return rv
 
 
-def write_symbols(out_dir, nmspace, name_file):
-    # File format is: Z, Sym, full name
-    with open(os.path.join(out_dir, "DefaultSymbols.cpp"),'w') as f:
-        f.write("#include \"SDE/NWXDefaults.hpp\"\n\n")
-        write_warning(__file__, f)
-        f.write("\nnamespace {} {{\n".format(nmspace))
-        f.write("using return_type = typename "
-                "ChemistryRuntime::at_sym_lut_type;\n")
-        f.write("return_type default_symbols("
-                "){\n")
-        f.write("    return_type rv;\n")
-        for line in open(name_file).readlines():
-            z, sym, name = line.strip().split()
-            f.write("    rv[\"{}\"] = {};\n".format(sym, z))
-        f.write("    return rv;\n}}\n}} // End namespace {}\n".format(nmspace))
+def parse_symbols(name_file, atoms):
+    for line in open(name_file).readlines():
+        z, sym, name = line.strip().split()
+        if not z in atoms:
+            atoms[z] = AtomicData()
+        atoms[z].sym = sym
+        atoms[z].name = name
+        atoms[z].Z = z
+    return atoms
 
-def write_isotopes(out_dir, nmspace, iso_file, iso_ab_file):
-    # Format for both files is:
-    #  Z, Sym, isotope number, average mass, lower and upper bounds
-    data = {}
-    for file_name in [iso_file, iso_ab_file]:
-        with open(file_name, 'r') as f:
-            for line in f.readlines()[5:]:
-                z, sym, isonum, mid = line.strip().split()[:4]
-                if not z in data:
-                    data[z] = {}
-                if file_name == iso_file:
-                    data[z][isonum] = IsotopeInfo(isonum, mid)
+def parse_isotopes(iso_file, atoms):
+    new_atom =  r"(\d{1,2})\s+[a-zA-Z]{1,2}\s+[a-zA-Z]+\s+"
+    new_iso = r"(\d+)\**\s+(\d+\.\d+)\s((\d+\s?)+)+"
+    new_atom+=new_iso
+    def par_fxn(match, atom):
+        as_str = match[1].replace(" ", "") + match[2].replace(" ", "")
+        atom.add_isotope(match[0], float(as_str))
+    with open(iso_file, 'r') as f:
+        Z = 0
+        for line in f.readlines():
+            if re.match( new_atom, line):
+                match = re.match(new_atom, line).groups()
+                Z = match[0]
+                par_fxn(match[1:], atoms[Z])
+            elif re.match(new_iso, line):
+                match = re.match(new_iso, line).groups()
+                par_fxn(match, atoms[Z])
+    return atoms
+
+def parse_mass(mass_file, atoms):
+    mass = r"((?:\d+\.\d+(?:\s\d+)*,*\s?)+)"
+    with open(mass_file, 'r') as f:
+        for line in f:
+            if re.search(mass, line):
+                Z = line.split()[0]
+                masses = re.search(mass, line).groups()[0].replace(" ","")
+                if len(masses.split(',')) == 2: # Has error bars
+                    temp = masses.split(',')
+                    atoms[Z].mass = (float(temp[0]) + float(temp[1]))/2.0
                 else:
-                    if isonum in data[z]:
-                        data[z][isonum].abundance = mid
-                    else:
-                        print("Warning: {} isotope {}".format(sym,isonum))
-    with open(os.path.join(out_dir, "DefaultIsotopes.cpp"), 'w') as f:
-        f.write("#include \"SDE/NWXDefaults.hpp\"\n")
-        write_warning(__file__,f)
-        f.write("namespace {} {{\n".format(nmspace))
-        f.write("using return_t = typename "
-                "ChemistryRuntime::isotope_lut_type;\n")
-        f.write("using isotope_list = typename "
-                "ChemistryRuntime::isotope_list;\n")
-        f.write("return_t default_isotopes(){\n")
-        f.write("    return_t rv;\n")
-        for z, isotopes in data.items():
-            f.write("    rv[{}] = isotope_list{{}};\n".format(z))
+                    atoms[Z].mass = float(masses)
+    return atoms
 
-            for isonum, iso_data in isotopes.items():
-                f.write("    rv[{}].push_back(".format(z))
-                iso_data.cxxify(f)
-                f.write(");\n")
-        f.write("    return rv;\n}\n")
-        f.write("}} // End namespace {}\n".format(nmspace))
+def write_ptable(out_dir, amu2me, atoms):
+    out_file = os.path.join(out_dir, "NWXPeriodicTablePIMPL.cpp")
+    in_file = "LibChemist/Implementations/PeriodicTablePIMPL.hpp"
+    sorted_keys = sorted([int(x) for x in atoms.keys()])
+    max_Z = sorted_keys[-1]
+    tab = "    "
+    with open(out_file, 'w') as f:
+        f.write(
+"""/* 
+ * This file has been autogenerated by generate_atomicinfo.py.  Any 
+ * modifications made to this file will be lost next time generate_atomicinfo.py
+ * is run.
+ */
 
-def write_atoms(out_dir, nmspace, mass_file, cov_file, vdw_file,
-                mult_file, pm2bohr):
-    atomicinfo = {}
-    # File format is: Z, Sym, average mass, lower and upper bounds to mass
-    for l in open(mass_file).readlines()[5:]:
-        z, sym, mid = l.strip().split()[:3]
-        if not z in atomicinfo:
-            atomicinfo[z] = AtomicInfo(z)
-        atomicinfo[z].mass = mid
+#include \"{}\"
+        
+namespace LibChemist::detail_ {{
 
-    # File format is: Z, radius, unit
-    for l in open(cov_file).readlines()[1:]:
-        z, r = l.strip().split()[:2]
-        atomicinfo[z].cov_radius = float(r)/pm2bohr
+class NWXPeriodicTablePIMPL : public PeriodicTablePIMPL {{
+public:
+    NWXPeriodicTablePIMPL() = default;
+protected:
+    using my_type = NWXPeriodicTablePIMPL;
+    NWXPeriodicTablePIMPL(const my_type& rhs) = default;
+private:
+    std::unique_ptr<PeriodicTablePIMPL> clone_() const override {{
+        return std::unique_ptr<my_type>(new my_type(*this));
+    }}            
+            
+    size_type max_Z_() const noexcept override {{
+        return {};
+    }}
+            
+    Atom get_atom_(size_type Z) const override {{
+        switch(Z) {{
+""".format(in_file, max_Z))
+        for k in sorted_keys:
+            ai = atoms[str(k)]
+            f.write("{}case({}): {{ return Atom(".format(tab*3, k))
+            f.write("{}ul, {}, \"{}\");}}\n".format(k, ai.mass*amu2me, ai.sym))
 
-    # File format is: Z, radius, unit
-    for l in open(vdw_file).readlines()[1:]:
-        z, r = l.strip().split()[:2]
-        atomicinfo[z].vdw_radius = float(r)/pm2bohr
+        f.write(
+"""            default : {{ throw std::out_of_range(\" Z > {}\"); }}
+        }}
+    }}
+            
+    Atom get_isotope_(size_type Z, size_type mass_num) const override {{
+        switch(Z) {{
+""".format(max_Z))
 
-    # File format: Z, configuration, multiplicity, term symbol
-    for l in open(mult_file).readlines()[5:]:
-        z, occ, mult = l.strip().split()[:3]
-        atomicinfo[z].mult = mult
+        for k in sorted_keys:
+            f.write("{}case({}): {{\n".format(tab*3, k)) #Start case 1
+            f.write("{}switch(mass_num) {{\n".format(tab*4)) #Start switch 2
+            ai = atoms[str(k)]
+            for mn in sorted([int(x) for x in ai.isotopes]):
+                mi = ai.isotope_masses[str(mn)] * amu2me
+                f.write("{}case({}): {{ return Atom(".format(tab*5, mn))
+                f.write("{}ul, {}, \"{}\");}}\n".format(k, mi, ai.sym))
+            f.write("{}}}\n".format(tab*4)) #Close switch 2
+            f.write("{}}}\n".format(tab*3)) #Close case 1
+        f.write(
+"""            default : { throw std::out_of_range(\"No isotope data\"); }
+        }
+    }
+    
+    isotope_list isotopes_(size_type Z) const override {
+        switch(Z) {
+""")
+        for k in sorted_keys:
+            f.write("{}case({}) : {{ return {{".format(tab*3, k))
+            ai = atoms[str(k)]
+            for mn in sorted([int(x) for x in ai.isotopes]):
+                f.write("{}, ".format(mn))
+            f.write("}; }\n")
+        f.write(
+"""            default : {{ throw std::out_of_range(\" Z > {}\"); }}
+        }}
+    }}        
+}};
+        
+std::unique_ptr<PeriodicTablePIMPL> nwx_default_ptable() {{
+    return std::make_unique<NWXPeriodicTablePIMPL>();
+}}
+}} // namespace LibChemist::detail_
+""".format(max_Z))
 
-    src_file = os.path.join(out_dir, "DefaultAtoms.cpp")
-    with open(src_file, 'w') as f:
-        f.write("#include \"SDE/NWXDefaults.hpp\"\n")
-        write_warning(__file__, f)
-        f.write("namespace {} {{\n".format(nmspace))
-        f.write("using return_t = typename "
-                "ChemistryRuntime::indexed_atom_type;\n")
-        f.write("return_t default_atoms() {\n")
-        f.write("    return_t rv;\n")
-        for k, v in sorted(atomicinfo.items()):
-            f.write("    rv[{}] ".format(k))
-            f.write("= LibChemist::Atom{std::array<double, 3>{0.0, 0.0, 0.0}, "
-                    "std::map<LibChemist::Atom::Property, double>{\n")
-            v.cxxify(f)
-            f.write("    }};\n")
-        f.write("    return rv;\n")
-        f.write("}\n} // End namespace SDE\n")
+def write_tests(out_dir, amu2me, atoms):
+    sorted_keys = sorted([int(x) for x in atoms.keys()])
+    max_Z = sorted_keys[-1]
+    with open(os.path.join(out_dir, "TestPeriodicTable.cpp"), 'w') as f:
+        f.write(
+"""/*
+* This file has been autogenerated by generate_atomicinfo.py.  Any changes made
+* to it will be lost next time generate_atomicinfo.py is run.
+*/
 
-def main():
-    # Knobs that you may want to tweak to control settings
-    pm2bohr = 52.917721067  # Conversion from picometers to Bohr
-    dest_dir = os.path.join("LibChemist", "Defaults") #Directory 4 output
-    nmspace = "SDE"  # Namespace for all C++ classes
+#include <LibChemist/PeriodicTable.hpp>
+#include <catch/catch.hpp>
 
+using namespace LibChemist;
+using size_type = typename PeriodicTable::size_type;
+using isotope_list = typename PeriodicTable::isotope_list;
+
+void test_ptable(const PeriodicTable& ptable) {{
+    REQUIRE(ptable.max_Z() == {});
+""".format(max_Z))
+        for Z in range(1, max_Z + 1):
+            ai = atoms[str(Z)]
+            f.write("    REQUIRE(ptable.get_atom({}) == Atom{{{}, \"{}\", "
+                    "{}ul}});\n".format(Z, ai.mass*amu2me, ai.sym, Z))
+            f.write("    REQUIRE(ptable.isotopes({}) == isotope_list{{"
+                    "".format(Z))
+            sorted_mn = sorted([int(mn) for mn in ai.isotopes])
+            for mn in sorted_mn:
+                f.write("{}, ".format(mn))
+            f.write("});\n")
+            for mn in sorted_mn:
+                mi = ai.isotope_masses[str(mn)]*amu2me
+                f.write("    REQUIRE(ptable.get_isotope({}, {}) == Atom{{{}ul, "
+                        "\"{}\", {}}});\n".format(Z, mn, Z, ai.sym, mi))
+        f.write(
+"""}
+
+TEST_CASE("PeriodicTable Class") {
+    SECTION("Typedefs") {
+        REQUIRE(std::is_same_v<size_type, std::size_t>);
+        REQUIRE(std::is_same_v<isotope_list, std::vector<size_type>>);
+    }
+    
+    PeriodicTable ptable;
+    SECTION("Default CTor") { 
+        test_ptable(ptable); 
+    }
+    SECTION("Copy CTor") {
+        PeriodicTable ptable2(ptable); 
+        test_ptable(ptable2);  
+    }
+    SECTION("Copy assign") {
+        PeriodicTable ptable2;
+        auto& pptable = (ptable2 = ptable);
+        test_ptable(ptable2);
+        REQUIRE(&pptable == &ptable2);
+    }
+    SECTION("Move CTor") {
+        PeriodicTable ptable2(std::move(ptable)); 
+        test_ptable(ptable2); 
+    }
+    SECTION("Move assign") {
+        PeriodicTable ptable2;
+        auto& pptable = (ptable2 = std::move(ptable));
+        test_ptable(ptable2);
+        REQUIRE(&pptable == &ptable2);
+    }
+}
+""")
+
+def main(dest_dir, amu2me):
     # Get and set some paths
     my_dir =os.path.dirname(os.path.realpath(__file__))
-    data_dir = os.path.join(my_dir, "physical_data")
-    out_dir = os.path.join(os.path.dirname(my_dir), dest_dir)
+    data_dir = os.path.join(my_dir, "physical_data") #Dir w/ files
+    out_dir = os.path.join(os.path.dirname(my_dir), dest_dir) #Dir to write to
     name_file = os.path.join(data_dir, "ElementNames.txt")
-    iso_file = os.path.join(data_dir, "CIAAW-ISOTOPEMASSES.formatted.txt")
-    iso_ab_file = os.path.join(data_dir, "CIAAW-ABUNDANCE.formatted.txt")
-    mass_file = os.path.join(data_dir, "CIAAW-MASSES.formatted.txt")
-    cov_file = os.path.join(data_dir, "CovRadii.txt")
-    vdw_file = os.path.join(data_dir, "VanDerWaalRadius.txt")
-    mult_file = os.path.join(data_dir, "NIST-ATOMICION.formatted.txt")
+    iso_file = os.path.join(data_dir, "CIAAW-ISOTOPEMASSES.txt")
+    mass_file = os.path.join(data_dir, "CIAAW-MASSES.txt")
+    #cov_file = os.path.join(data_dir, "CovRadii.txt")
+    #vdw_file = os.path.join(data_dir, "VanDerWaalRadius.txt")
+    #mult_file = os.path.join(data_dir, "NIST-ATOMICION.formatted.txt")
 
-    write_symbols(out_dir, nmspace, name_file)
-    write_isotopes(out_dir, nmspace, iso_file, iso_ab_file)
-    write_atoms(out_dir, nmspace, mass_file, cov_file, vdw_file, mult_file,
-                pm2bohr)
+    atoms = parse_symbols(name_file, {})
+    atoms = parse_isotopes(iso_file, atoms)
+    atoms = parse_mass(mass_file, atoms)
+
+    write_ptable(out_dir, amu2me, atoms)
+    write_tests(os.path.join(os.path.dirname(my_dir), "LibChemist_Test"),
+                amu2me, atoms)
 
 if __name__ == '__main__' :
-    main()
+    # Knobs that you may want to tweak to control settings
+    amu2me = 1822.888486192 # ratio of mass of electron to a Dalton
+    dest_dir = os.path.join("LibChemist", "Defaults") #Directory 4 output
+    main(dest_dir, amu2me)
 
