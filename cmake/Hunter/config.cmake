68517e46246ecf3e0e47b6d3f56d5bd06c0a0355
ryanmrichard <ryanmrichard1@gmail.com>
2018-08-02 16:23:33 -0500

    Moves prop types over and starts hooking TAMM in

--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -1,9 +1,32 @@
-set(gh_url "https://api.github.com/repos/NWChemEx-Project/Utilities")
+set(gh_url "https://api.github.com/repos/NWChemEx-Project")
 set(token "d28e8df2f89944f49e7144c747ffe3a114547469")
-hunter_config(
-        Utilities
+nwx_depend(
+        NAME Utilities
         VERSION 0.0.0
-        URL "${gh_url}/tarball/master?access_token=${token}"
-        SHA1 "c3c7d10c77b7744a70f1423e50e0cd039d2064f5"
+        URL "${gh_url}/Utilities/tarball/master?access_token=${token}"
+        SKIP_SHA1
         CMAKE_ARGS "BUILD_TESTS=off"
+)
+hunter_config(
+        bphash
+        VERSION 1.1
+        URL "https://github.com/bennybp/BPHash/archive/v1.1.tar.gz"
+        SHA1 "88e14eda8b1f8224caeae8c43407770a651e027e"
+        CMAKE_ARGS "BUILD_SHARED_LIBS=True"
+)
+
+nwx_depend(
+    NAME SDE
+    VERSION 0.0.0
+    URL "${gh_url}/SDE/tarball/master?access_token=${token}"
+    SKIP_SHA1
+    CMAKE_ARGS "BUILD_TESTS=off"
+)
+
+nwx_depend(
+        NAME TAMM
+        VERSION 0.0.0
+        URL "${gh_url}/TAMM/tarball/alloc?access_token=${token}"
+        SKIP_SHA1
+        CMAKE_ARGS "CMAKE_PREFIX_PATH=${CMAKE_PREFIX_PATH}"
 )
