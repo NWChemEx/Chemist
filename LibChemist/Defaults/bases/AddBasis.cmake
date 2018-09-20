b1387bf7c3b022a49e7635d741a523a7390c1399
ryanmrichard <ryanmrichard1@gmail.com>
2018-09-20 12:23:03 -0500

    Facilitates the addition of additional basis sets

---
+++ LibChemist/Defaults/bases/AddBasis.cmake
@@ -0,0 +1,6 @@
+set(
+    LIBCHEMIST_BASIS_SOURCE
+    Defaults/bases/cc-pvdz.cpp
+    Defaults/bases/sto-3g.cpp
+    Defaults/bases/cc-pvdz-jkfit.cpp
+)
--- cmake/Hunter/config.cmake
+++ cmake/Hunter/config.cmake
@@ -1,9 +1,9 @@
 set(gh_url "https://api.github.com/repos/NWChemEx-Project/Utilities")
-set(token "d28e8df2f89944f49e7144c747ffe3a114547469")
-hunter_config(
-        Utilities
+set(token "a869897b4a98f43589ca5af1cc2b599f973b8619")
+nwx_depend(
+        NAME Utilities
         VERSION 0.0.0
         URL "${gh_url}/tarball/master?access_token=${token}"
-        SHA1 "c3c7d10c77b7744a70f1423e50e0cd039d2064f5"
+        SKIP_SHA1
         CMAKE_ARGS "BUILD_TESTS=off"
 )
