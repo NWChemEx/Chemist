5cf09952b73af9a2b1ca87768c4bb1ce9833ba2a
ryanmrichard <ryanmrichard1@gmail.com>
2019-10-07 11:24:45 -0500

    starts shell

--- libchemist/basis_set/shell/detail_/shell_pimpl.hpp
+++ libchemist/basis_set/shell/detail_/shell_pimpl.hpp
@@ -1,18 +1,35 @@
 #pragma once
+#include "libchemist/basis_set/contracted_gaussian/detail_/cgto_pimpl.hpp"
 
 namespace libchemist::detail_ {
 
 template<typename T>
 class ShellPIMPL {
+public:
+    std::unique_ptr<AliasingCGTOPIMPL<T>> cgto(size_type i) {
+        auto& old_cs      = coefs(i);
+        auto& old_as      = exps(i);
+        const auto nprims = old_cs.size();
+        std::vector<T*> cs(nprims);
+        std::vector<T*> as(nprims);
+        for(decltype(nprims) i = 0; i < nprims; ++i) {
+            cs[i] = &old_cs[i];
+            as[i] = &old_as[i];
+        }
+        return std::make_unique<AliasingCGTOPIMPL<T>>(std::move(cs),
+                                                      std::move(as));
+    }
+
 private:
     utilities::OwnOrBorrow<bool> m_pure_;
     utilities::OwnOrBorrow<int> m_l_;
 };
 
-template<typename T, typename Container>
+template<typename T, template<typename> Container>
 class ShellPIMPL_ {
 private:
-    Container
+    Container<MathSet<T>> m_coefs_;
+    Container<MathSet<T>> m_exps_;
 };
 
 template<
