98e42d7eba6edbb69a3e9b2f7a6b93b5a40bf71f
Ryan Richard <ryanmrichard1@gmail.com>
2020-03-05 15:12:16 -0600

    [WIP] Moves ta_helpers to libchemist (#97)
    * moves ta_helpers to libchemist
    
    * einsum appears to work
    
    * remove gitlab CI

---
+++ tests/ta_helpers/einsum/detail_/index_map.cpp
@@ -0,0 +1,55 @@
+#include <catch2/catch.hpp>
+#include <libchemist/ta_helpers/einsum/detail_/index_map.hpp>
+
+using namespace libchemist::einsum;
+using libchemist::einsum::detail_::IndexMap;
+
+/* We know splitting strings works from parse_index test so we only need to make
+ * sure that the inputs are correctly fed through parse_index and saved
+ * correctly.
+ */
+TEST_CASE("IndexMap::IndexMap(string, string, string)") {
+    IndexMap im("i, j, k", "i, k, l", "j, l");
+
+    SECTION("Result indices") {
+        types::index_set corr{"i", "j", "k"};
+        REQUIRE(im.result_vars() == corr);
+    }
+
+    SECTION("LHS indices") {
+        types::index_set corr{"i", "k", "l"};
+        REQUIRE(im.lhs_vars() == corr);
+    }
+
+    SECTION("RHS indices") {
+        types::index_set corr{"j", "l"};
+        REQUIRE(im.rhs_vars() == corr);
+    }
+}
+
+TEST_CASE("IndexMap::select_result") {
+    IndexMap im("i,j", "j", "i");
+
+    std::map<std::string, std::size_t> qs{{"i", 0}, {"j", 1}, {"k", 2}};
+
+    std::vector<std::size_t> corr{0, 1};
+    REQUIRE(im.select_result(qs) == corr);
+}
+
+TEST_CASE("IndexMap::select_lhs") {
+    IndexMap im("j", "i,j", "i");
+
+    std::map<std::string, std::size_t> qs{{"i", 0}, {"j", 1}, {"k", 2}};
+
+    std::vector<std::size_t> corr{0, 1};
+    REQUIRE(im.select_lhs(qs) == corr);
+}
+
+TEST_CASE("IndexMap::select_rhs") {
+    IndexMap im("i", "j", "j,i");
+
+    std::map<std::string, std::size_t> qs{{"i", 0}, {"j", 1}, {"k", 2}};
+
+    std::vector<std::size_t> corr{1, 0};
+    REQUIRE(im.select_rhs(qs) == corr);
+}
---
+++ tests/ta_helpers/einsum/einsum.cpp
@@ -0,0 +1,52 @@
+#include <catch2/catch.hpp>
+#include <libchemist/ta_helpers/einsum/einsum.hpp>
+#include <libchemist/ta_helpers/ta_helpers.hpp>
+
+using namespace libchemist::einsum;
+
+TEST_CASE("einsum"){
+
+  auto& world = TA::get_default_world();
+
+  TA::TSpArrayD vec(world, {1.0, 2.0, 3.0});
+  TA::TSpArrayD sq_mat(world, {{1.0, 2.0, 3.0},
+                              {4.0, 5.0, 6.0},
+                              {7.0, 8.0, 9.0}});
+  TA::TSpArrayD mat(world, {{1.0, 2.0, 3.0},
+                           {4.0, 5.0, 6.0}});
+  TA::TSpArrayD sq_tensor(world, {{{1.0, 2.0, 3.0},
+                                  {4.0, 5.0, 6.0},
+                                  {7.0, 8.0, 9.0}},
+
+                                 {{10.0, 11.0, 12.0},
+                                  {13.0, 14.0, 15.0},
+                                  {16.0, 17.0, 18.0}},
+
+                                 {{19.0, 20.0, 21.0},
+                                  {22.0, 23.0, 24.0},
+                                  {25.0, 26.0, 27.0}}});
+
+  TA::TSpArrayD tensor(world, {{{1.0, 2.0, 3.0},
+                               {4.0, 5.0, 6.0},
+                               {7.0, 8.0, 9.0}},
+
+                              {{10.0, 11.0, 12.0},
+                               {13.0, 14.0, 15.0},
+                               {16.0, 17.0, 18.0}}});
+
+    SECTION("tensor-tensor"){
+      auto result = einsum("i,j,k", "i, j, k", "l, j, k", sq_tensor, tensor);
+      TA::TSpArrayD corr(world, {{{11,26,45,},
+                                  {68,95,126,},
+                                  {161,200,243,}},
+
+                                 {{110,143,180,},
+                                  {221,266,315,},
+                                  {368,425,486,}},
+
+                                  {{209,260,315,},
+                                   {374,437,504,},
+                                   {575,650,729,}}});
+      REQUIRE(libchemist::allclose(result, corr));
+    }
+} // einsum
