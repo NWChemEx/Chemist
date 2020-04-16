c104403c9ec753bc5e6b8bef22aaa139edefd644
jboschen <jboschen@iastate.edu>
2020-04-16 12:34:03 -0500

    Some tests for allclose

--- tests/ta_helpers/ta_helpers.cpp
+++ tests/ta_helpers/ta_helpers.cpp
@@ -3,7 +3,23 @@
 
 using tensor_type = TA::TSpArrayD;
 
-TEST_CASE("TA Helpers") {
+TEST_CASE("allclose") {
+    auto& world = TA::get_default_world();
+    tensor_type one(world,{{1.0,1.0},{1.0,1.0}});
+    tensor_type almost_one(world,{{1.001,1.001},{1.001,1.001}});
+    tensor_type zero(world,{{0.0,0.0},{0.0,0.0}});
+    tensor_type small(world,{{1.0E-20,1.0E-20},{1.0E-20,1.0E-20}});
+
+    REQUIRE(libchemist::allclose(one,one));
+
+    REQUIRE_FALSE(libchemist::allclose(one,almost_one));
+    REQUIRE(libchemist::allclose(one,almost_one,1.0E-2));
+
+    REQUIRE(libchemist::allclose(zero,small));
+    REQUIRE_FALSE(libchemist::allclose(zero,small,1.0E-5,1.0E-21));
+}
+
+TEST_CASE("Elementwise helpers") {
 
     auto& world = TA::get_default_world();
     tensor_type matrix(world,{{1.0,2.0},{3.0,4.0}});
