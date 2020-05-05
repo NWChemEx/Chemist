c3ba1f17729a3e726fc6451491af2059e62d855e
Jonathan M. Waldrop <relik107@gmail.com>
2020-05-05 14:55:15 -0500

    Use regular case.

--- libchemist/ta_helpers/detail_/contraction_dummy_annotation.hpp
+++ libchemist/ta_helpers/detail_/contraction_dummy_annotation.hpp
@@ -5,22 +5,22 @@ namespace libchemist::detail_ {
 
 // Makes indices for the contraction of one dimension of an arbitrarily ranked tensor with a matrix
 inline std::tuple<std::string, std::string, std::string>
-        contraction_dummy_annotations(std::size_t DIM, std::size_t TARGET_DIM) {
+        contraction_dummy_annotations(std::size_t dim, std::size_t target_dim) {
 
-    auto beginning = TA::detail::dummy_annotation(DIM);
+    auto beginning = TA::detail::dummy_annotation(dim);
 
     std::ostringstream final;
-    if (DIM > 0) {
-        if (TARGET_DIM == 0) { final << "iX"; }
+    if (dim > 0) {
+        if (target_dim == 0) { final << "iX"; }
         else { final << "i0"; }
     }
-    for (unsigned int d = 1; d < DIM; ++d) {
-        if (d == TARGET_DIM) { final << ",iX"; }
+    for (unsigned int d = 1; d < dim; ++d) {
+        if (d == target_dim) { final << ",iX"; }
         else { final << ",i" << d; }
     }
 
     std::ostringstream switcher;
-    switcher << "i" << TARGET_DIM << ",iX";
+    switcher << "i" << target_dim << ",iX";
 
     return {beginning, final.str(), switcher.str()};
 }
