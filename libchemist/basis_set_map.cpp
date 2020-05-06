223a7527db98502243370d22cdef2058df23f9ac
jboschen <jboschen@iastate.edu>
2020-05-06 10:25:53 -0500

    Remove commented out lines

--- libchemist/basis_set_map.cpp
+++ libchemist/basis_set_map.cpp
@@ -59,33 +59,6 @@ BasisSetMap<T>::BasisSetMap(const AOBasisSet<T>& bs) {
         }
         a2s_offset += center.size();
     }
-
-//    auto prev_coords = bs[0].center();
-//
-//    size_type shell_beg   = 0;
-//    size_type shell_end   = 0;
-//    size_type s2ao_offset = 0;
-//    size_type atom        = 0;
-//    for(auto shell : bs) {
-//        s2ao_vec.push_back(
-//                std::make_pair(s2ao_offset, s2ao_offset + shell.size()));
-//
-//        for(size_type idx = s2ao_offset; idx < s2ao_offset + shell.size();
-//            idx++) {
-//            ao2s_vec.push_back(shell_end);
-//        }
-//
-//        s2ao_offset += shell.size();
-//        if(shell.center() != prev_coords) {
-//            a2s_vec.push_back(std::make_pair(shell_beg, shell_end));
-//            prev_coords = shell.center();
-//            shell_beg   = shell_end;
-//            ++atom;
-//        }
-//        s2a_vec.push_back(atom);
-//        shell_end++;
-//    }
-//    a2s_vec.push_back(std::make_pair(shell_beg, shell_end));
 }
 
 template class BasisSetMap<double>;
