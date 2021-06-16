be7da4e6f0d4a24617b584faeddf3af0211319c6
ryan <ryanmrichard1@gmail.com>
2021-06-16 09:36:13 -0500

    try new get CMake mechanism

--- .github/workflows/scripts/build_and_test.sh
+++ .github/workflows/scripts/build_and_test.sh
@@ -22,9 +22,12 @@ set -e # Exit with error if any command fails
 
 arch=Linux-x86_64
 cmake_root=$(pwd)/cmake-"${cmake_version}"-"${arch}"
-cmake_command="${cmake_root}/bin/cmake"
-ctest_command="${cmake_root}/bin/ctest"
+cmake_command=cmake #"${cmake_root}/bin/cmake"
+ctest_command=ctest #"${cmake_root}/bin/ctest"
 toolchain_file=$(pwd)/toolchain.cmake
+
+which cmake
+cmake --version
 
 #Step 1: Write toolchain.cmake
 # TODO: Do we really need all this? I Just took what was in all the old files
