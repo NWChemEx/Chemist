44f1d3b1a7a36de97e05385f365349dbea6317b4
Ryan Richard <ryanmrichard1@gmail.com>
2021-06-26 11:02:02 -0500

    back-up of progress so far

--- include/libchemist/set_theory/subset.hpp
+++ include/libchemist/set_theory/subset.hpp
@@ -5,35 +5,74 @@ namespace libchemist::set_theory {
 template<typename SetType>
 class FamilyOfSets;
 
-/** @brief View of a set which includes a subset of that set's members.
+/** @brief Models a subset of a set.
  *
+ *  Given a set @f$\mathbb{S}@f$, @f$\mathbb{T}@f$ is a subset of
+ *  @f$\mathbb{S}@f$ if @f$\mathbb{T}@f$ is a set and if every member of
+ *  @f$\mathbb{T}@f$ is also a member of @f$\mathbb{S}@f$. @f$\mathbb{T}@f$ is a
+ *  proper subset if at least one member of @f$\mathbb{S}@f$ does not appear in
+ *  @f$\mathbb{T}@f$.
  *
+ *  Programatically, this class serves as a proxy for an object of type
+ *  @p SetType whose members are restricted to being a subset of another
+ *  @p SetType instance. This proxy is a view/mask of the @p SetType instance
+ *  with limited @p SetType functionality (mainly the ability to determine if
+ *  members are in the proxy and the ability to iterate over the members of the
+ *  proxy).
+ *
+ *  @note Using a Subset instance after the parent FamilyOfSets goes out of
+ *        scope is undefined behavior and will likely result in a segfault.
  *
  *  @tparam SetType Type of the parent set this Subset instance is a subset of.
  */
 template<typename SetType>
 class Subset {
 private:
-    using my_type     = Subset<SetType>;
+    /// Type of an instance of this class
+    using my_type = Subset<SetType>;
+
+    /// Type providing the API for interacting with a SetType object
     using traits_type = SetTraits<SetType>;
 
 public:
-    using parent_type     = FamilyOfSets<SetType>;
-    using value_type      = typename traits_type::value_type;
+    /// Type of the object which this is a view/subset of
+    using parent_type = FamilyOfSets<SetType>;
+
+    /// Type of the elements in SetType
+    using value_type = typename traits_type::value_type;
+
+    /// Type of a read-only reference to an element of a SetType object
     using const_reference = typename traits_type::const_reference;
-    using size_type       = typename traits_type::size_type;
-    using object_type     = SetType;
-    using obj_reference   = object_type&;
-    using const_obj_ref   = const object_type&;
+
+    /// Type that SetType uses for indexing and offsets
+    using size_type = typename traits_type::size_type;
+
+    /// Type of the object this is a view of
+    using object_type = SetType;
+
+    /// Type of a read-only reference to the object this is a view of
+    using const_obj_ref = const object_type&;
 
     /** @brief Creates a new, empty, subset of @p parent
      *
-     *  This ctor creates a new subset of @p parent. The subset will be empty.
-     *  Elements can be added by calling `insert`.
+     *  This ctor creates a new subset of @p parent. The subset will be empty if
+     *  no initializer list is provided. Additional elements can be added by
+     *  calling `insert`.
+     *
+     *  @tparam ElemType The type used to specify elements of @p parent.
+     *                   @p ElemType is expected to be implicitly convertible to
+     *                   either size_type or to value_type.
      *
      *  @param[in] parent The set that this is a subset of.
+     *  @param[in] il The elements to populate the subset with. Defaults to an
+     *                empty list, which in turn creates an empty subset.
+     *
+     *  @throw std::out_of_range if any of the elements in @p il are not in
+     *                           @p parent. Weak throw guarantee.
      */
-    explicit Subset(const parent_type& parent) : m_parent_(parent) {}
+    template<typename ElemType = size_type>
+    explicit Subset(const parent_type& parent,
+                    std::initializer_list<ElemType> il = {});
 
     /** @brief The number of elements in this Subset.
      *
@@ -46,32 +85,157 @@ public:
      *  @throw None No throw guarantee.
      */
     size_type size() const noexcept { return m_members_.size(); }
+
+    /** @brief Determines if this subset is empty or not.
+     *
+     *  This function is a convenience function for determining if the size of
+     *  this set is zero.
+     *
+     *  @return True if this set is empty (zero elements) and false otherwise.
+     *
+     *  @throw None No throw gurantee.
+     */
     bool empty() const noexcept { return m_members_.empty(); }
 
-    void insert(size_type i);
-    void insert(const_reference elem);
-
+    /** @brief Returns an element of this subset by offset.
+     *
+     *  This function can be used to retrieve an element of the subset by
+     *  offset. It behave similar to `operator[]` with the addition of a bounds
+     *  check on @p i. If you know that @p i is in bounds it is recommended that
+     *  you call `operator[]` instead.
+     *
+     *  @param[in] i The offset of the requested element. @p i must be in the
+     *               range [0, size()).
+     *
+     *  @return A read-only reference to the requested object.
+     *
+     *  @throw std::out_of_range if @p i is not in the range [0, size()). Strong
+     *                           throw guarantee.
+     */
     const_reference at(size_type i) const;
+
+    /** @brief Returns an element of this subset by offset.
+     *
+     *  This function works similar to `at` except there is no bounds check. If
+     *  @p i is out of bounds a seg fault will likely occur. If you are unsure
+     *  of whether @p i is in bounds use `at`.
+     *
+     *  @param[in] i Which of elements in this subset to return. @p i must be in
+     *               the range [0,size()); however, no bounds check occurrs.
+     *
+     *  @return A read-only reference to the @p i -th element in the subset.
+     *
+     *  @throw None No throw guarantee.
+     */
     const_reference operator[](size_type i) const;
+
+    /** @brief Returns the number of times @p elem appears in this subset.
+     *
+     *  This function allows you to determine how many times the object @p elem
+     *  appears in this Subset. Since the @p elem may appear at most once in
+     *  this set, the result is either 0 or 1, i.e., true if @p elem is in the
+     *  set and false if it is not. No check is made to ensure that @p elem is
+     *  actually in the parent set.
+     *
+     *  @param[in] elem The element we are looking for.
+     *
+     *  @return True if @p elem is in the set and false otherwise.
+     *
+     *  @throw None No throw guarantee.
+     */
+    bool count(const_reference elem) const;
+
+    /** @brief Returns a read-only reference to the parent set.
+     *
+     *  This accessor can be used to retrieve the parent set that this instance
+     *  is a subset of. N.B. this is the object living in the FamilyOfSets not
+     *  the FamilyOfSets instance.
+     *
+     *  @return A read-only reference to the parent set.
+     *
+     *  @throw None No throw guarantee.
+     */
+    const_obj_ref object() const { return m_parent_->object(); }
+
+    /** @brief Inserts an element by offset in the parent set.
+     *
+     *  This function provides a mechanism for adding elements of the parent set
+     *  to this subset by using the offset of the element. The offset is the
+     *  offset in the parent set. Repeated insertions of an element do not
+     *  change the state of the Subset.
+     *
+     *  @param[in] i The offset of the element we are inserting. Must be in the
+     *               range [0, traits_type::size(object())).
+     *
+     *  @throw std::out_of_range if @p i is not in the range
+     *                           [0,traits_type::size(object())). Strong throw
+     *                           guarantee.
+     *
+     *  @warning Be very careful using insert when the parent set is filled with
+     *           objects of type `size_type`. In this case the only distinction
+     *           between insertion by offset and insertion by value is whether
+     *           or not the argument is a reference or value. This is
+     *           particularly tricky given implicit conversion rules. That all
+     *           said using the FamilyOfSets/Subset classes for a container
+     *           filled with objects of `size_type` is more costly than actually
+     *           having copies on account of the additional indirection. Hence
+     *           this is considered a sufficiently rare edge-case that we are
+     *           okay with the weird semantics for it.
+     */
+    void insert(size_type i);
+
+    /** @brief Inserts an element by value.
+     *
+     *  This function allows you to insert the specified element into the
+     *  subset. The element must appear in the parent set. Repeated insertions
+     *  of the same element do not change the state of the Subset.
+     *
+     *  @param[in] elem The element to add to this subset.
+     *
+     *  @throw std::out_of_range if @p elem is not found in object(). Strong
+     *                           throw guarantee.
+     *
+     *  @warning Be very careful using insert when the parent set is filled with
+     *           objects of type `size_type`. In this case the only distinction
+     *           between insertion by offset and insertion by value is whether
+     *           or not the argument is a reference or value. This is
+     *           particularly tricky given implicit conversion rules. That all
+     *           said using the FamilyOfSets/Subset classes for a container
+     *           filled with objects of `size_type` is more costly than actually
+     *           having copies on account of the additional indirection. Hence
+     *           this is considered a sufficiently rare edge-case that we are
+     *           okay with the weird semantics for it.
+     */
+    void insert(const_reference elem);
 
     // my_type operator+(const my_type& rhs) const;
     // my_type operator^(const my_type& rhs) const;
     // my_type operator-(const my_type& rhs) const;
 
-    const auto& object() const { return m_parent_.object(); }
-
 private:
+    /// Verifies that the size of object() is greater than @p i
     void obj_bounds_check_(size_type i) const;
+
+    /// Verifies that the size of this subset is greater than @p i
     void set_bounds_check_(size_type i) const;
+
+    /// The indices of the elements of object() in this subset
     boost::container::flat_set<size_type> m_members_;
 
-    /// The set we are a subset of
-    const parent_type& m_parent_;
+    /// Pointer to the family of sets we belong to
+    const parent_type* m_parent_;
 };
 
 // ---------------------------- Implementations --------------------------------
 
 #define SUBSET Subset<SetType>
+
+template<typename SetType>
+template<typename ElemType>
+SUBSET::Subset(const parent_type& parent, std::initializer_list<ElemType> il) :
+  m_parent_(&parent) {
+    for(const auto& i : il) insert(i);
+}
 
 template<typename SetType>
 void SUBSET::insert(size_type i) {
@@ -93,8 +257,15 @@ typename SUBSET::const_reference SUBSET::at(size_type i) const {
 
 template<typename SetType>
 typename SUBSET::const_reference SUBSET::operator[](size_type i) const {
-    const auto i_maps_to = m_members_[i];
+    const auto i_maps_to = *(m_members_.begin() + i);
     return traits_type::get_elem_i(object(), i_maps_to);
+}
+
+template<typename SetType>
+bool SUBSET::count(const_reference elem) const {
+    for(size_type i = 0; i < size(); ++i)
+        if((*this)[i] == elem) return true;
+    return false;
 }
 
 template<typename SetType>
--- tests/set_theory/subset.cpp
+++ tests/set_theory/subset.cpp
@@ -1,9 +1,43 @@
+#include "libchemist/set_theory/family_of_sets.hpp"
 #include "libchemist/set_theory/subset.hpp"
 #include "test_set_theory.hpp"
 
 TEMPLATE_LIST_TEST_CASE("Subset", "", container_types) {
-    using family_type = FamilyOfSets<TestType>;
+    // Work out some types we'll need for the unit tests
     using subset_type = Subset<TestType>;
+    using family_type = typename subset_type::parent_type;
+
+    SECTION("Typedefs") {
+        SECTION("parent_type") {
+            using corr = FamilyOfSets<TestType>;
+            STATIC_REQUIRE(std::is_same_v<family_type, corr>);
+        }
+        SECTION("value_type") {
+            using value_type = typename subset_type::value_type;
+            using corr       = typename TestType::value_type;
+            STATIC_REQUIRE(std::is_same_v<value_type, corr>);
+        }
+        SECTION("const_reference") {
+            using const_reference = typename subset_type::const_reference;
+            using corr            = const typename TestType::value_type&;
+            STATIC_REQUIRE(std::is_same_v<const_reference, corr>);
+        }
+        SECTION("size_type") {
+            using size_type = typename subset_type::size_type;
+            using corr      = typename TestType::size_type;
+            STATIC_REQUIRE(std::is_same_v<size_type, corr>);
+        }
+        SECTION("object_type") {
+            using object_type = typename subset_type::object_type;
+            using corr        = TestType;
+            STATIC_REQUIRE(std::is_same_v<object_type, corr>);
+        }
+        SECTION("const_obj_ref") {
+            using const_obj_ref = typename subset_type::const_obj_ref;
+            using corr          = const TestType&;
+            STATIC_REQUIRE(std::is_same_v<const_obj_ref, corr>);
+        }
+    }
 
     // Make a default/empty and non-default parent set
     TestType default_obj;
@@ -18,27 +52,175 @@ TEMPLATE_LIST_TEST_CASE("Subset", "", container_types) {
 
     // Make some subsets for the non-default parent set
     subset_type empty_non_defaulted(non_default);
-    subset_type e0(non_default);
-    subset_type e2(non_default);
-    subset_type e01(non_default);
-    e0.insert(0);
-    e2.insert(2);
-    e01.insert(0);
-    e01.insert(1);
+    subset_type e0(non_default, {0ul});
+    subset_type e2(non_default, {2ul});
+    subset_type e01(non_default, {0ul, 1ul});
 
-    SECTION("size()") {
-        REQUIRE(empty_defaulted.size() == 0);
-        REQUIRE(empyty_non_defaulted.size() == 0);
-        REQUIRE(e0.size() == 1);
-        REQUIRE(e2.size() == 1);
-        REQUIRE(e01.size() == 2);
+    SECTION("CTors/Assignment") {
+        SECTION("Initializer list cTor") {
+            SECTION("Default il") {
+                REQUIRE(empty_defaulted.empty());
+                REQUIRE(empty_defaulted.size() == 0);
+                REQUIRE(empty_defaulted.object() == default_obj);
+
+                REQUIRE(empty_non_defaulted.empty());
+                REQUIRE(empty_non_defaulted.size() == 0);
+                REQUIRE(empty_non_defaulted.object() == non_default_obj);
+            }
+
+            SECTION("Non-default il") {
+                REQUIRE_FALSE(e0.empty());
+                REQUIRE(e0.size() == 1);
+                REQUIRE(e0[0] == non_default_obj[0]);
+                REQUIRE(e0.object() == non_default_obj);
+
+                REQUIRE_FALSE(e2.empty());
+                REQUIRE(e2.size() == 1);
+                REQUIRE(e2[0] == non_default_obj[2]);
+                REQUIRE(e2.object() == non_default_obj);
+
+                REQUIRE_FALSE(e01.empty());
+                REQUIRE(e01.size() == 2);
+                REQUIRE(e01[0] == non_default_obj[0]);
+                REQUIRE(e01[1] == non_default_obj[1]);
+                REQUIRE(e01.object() == non_default_obj);
+
+                SECTION("Throws if invalid index") {
+                    REQUIRE_THROWS_AS(subset_type(non_default, {3ul}),
+                                      std::out_of_range);
+                }
+            }
+
+            SECTION("Copy CTor") {
+                subset_type copy(e0);
+                REQUIRE(copy.size() == 1);
+                REQUIRE(copy[0] == non_default_obj[0]);
+                REQUIRE(copy.object() == non_default_obj);
+            }
+
+            SECTION("Move CTor") {
+                subset_type moved(std::move(e0));
+                REQUIRE(moved.size() == 1);
+                REQUIRE(moved[0] == non_default_obj[0]);
+                REQUIRE(moved.object() == non_default_obj);
+            }
+
+            SECTION("Copy Assignment") {
+                subset_type copy(defaulted);
+                auto pcopy = &(copy = e0);
+                REQUIRE(pcopy == &copy);
+                REQUIRE(copy.size() == 1);
+                REQUIRE(copy[0] == non_default_obj[0]);
+                REQUIRE(copy.object() == non_default_obj);
+            }
+
+            SECTION("Move Assignment") {
+                subset_type moved(defaulted);
+                auto pmoved = &(moved = std::move(e0));
+                REQUIRE(pmoved == &moved);
+                REQUIRE(moved.size() == 1);
+                REQUIRE(moved[0] == non_default_obj[0]);
+                REQUIRE(moved.object() == non_default_obj);
+            }
+        }
     }
 
-    SECTION("empty()") {
-        REQUIRE(empty_defaulted.empty());
-        REQUIRE(empty_non_defaulted.empty());
-        REQUIRE_FALSE(e0.empty());
-        REQUIRE_FALSE(e2.empty());
-        REQUIRE_FALSE(e01.empty());
+    SECTION("accessors") {
+        SECTION("size()") {
+            REQUIRE(empty_defaulted.size() == 0);
+            REQUIRE(empty_non_defaulted.size() == 0);
+            REQUIRE(e0.size() == 1);
+            REQUIRE(e2.size() == 1);
+            REQUIRE(e01.size() == 2);
+        }
+
+        SECTION("empty()") {
+            REQUIRE(empty_defaulted.empty());
+            REQUIRE(empty_non_defaulted.empty());
+            REQUIRE_FALSE(e0.empty());
+            REQUIRE_FALSE(e2.empty());
+            REQUIRE_FALSE(e01.empty());
+        }
+
+        SECTION("at") {
+            SECTION("Throws if index is out of bounds") {
+                REQUIRE_THROWS_AS(empty_defaulted.at(0), std::out_of_range);
+            }
+            SECTION("Returns by reference") {
+                REQUIRE(&(e0.at(0)) == &(non_default.object()[0]));
+                REQUIRE(&(e2.at(0)) == &(non_default.object()[2]));
+                REQUIRE(&(e01.at(0)) == &(non_default.object()[0]));
+                REQUIRE(&(e01.at(1)) == &(non_default.object()[1]));
+            }
+        }
+
+        SECTION("operator[]") {
+            // Check addresses to ensure they're not copies
+            REQUIRE(&(e0[0]) == &(non_default.object()[0]));
+            REQUIRE(&(e2[0]) == &(non_default.object()[2]));
+            REQUIRE(&(e01[0]) == &(non_default.object()[0]));
+            REQUIRE(&(e01[1]) == &(non_default.object()[1]));
+        }
+
+        SECTION("count(elem_type)") {
+            SECTION("Object is in subset (and parent set)") {
+                REQUIRE(e0.count(non_default_obj[0]));
+            }
+            SECTION("Object is in parent set, but not in subset") {
+                REQUIRE_FALSE(e0.count(non_default_obj[1]));
+            }
+            SECTION("Object is not in parent set and thus not in subset") {
+                typename subset_type::value_type v;
+                const auto& v_ref = v;
+                REQUIRE_FALSE(e0.count(v_ref));
+            }
+        }
+
+        SECTION("object") {
+            REQUIRE(empty_defaulted.object() == default_obj);
+            REQUIRE(empty_non_defaulted.object() == non_default_obj);
+            REQUIRE(e0.object() == non_default_obj);
+            REQUIRE(e2.object() == non_default_obj);
+            REQUIRE(e01.object() == non_default_obj);
+        }
+    }
+
+    SECTION("insert(size_type)") {
+        SECTION("Throws if out of bounds") {
+            using except_t = std::out_of_range;
+            REQUIRE_THROWS_AS(empty_defaulted.insert(0), except_t);
+        }
+
+        SECTION("Add unique value") {
+            empty_non_defaulted.insert(1ul);
+            REQUIRE(empty_non_defaulted.size() == 1);
+            REQUIRE(&(empty_non_defaulted[0]) == &(non_default.object()[1]));
+        }
+
+        SECTION("Adding duplicate value does nothing") {
+            e0.insert(0ul);
+            REQUIRE(e0.size() == 1);
+            REQUIRE(&(e0[0]) == &(non_default.object()[0]));
+        }
+    }
+
+    SECTION("insert(elem_type)") {
+        auto& i0 = non_default.object()[0];
+        SECTION("Throws if out of bounds") {
+            using except_t = std::out_of_range;
+            REQUIRE_THROWS_AS(empty_defaulted.insert(i0), except_t);
+        }
+
+        SECTION("Add unique value") {
+            empty_non_defaulted.insert(non_default.object()[1]);
+            REQUIRE(empty_non_defaulted.size() == 1);
+            REQUIRE(&(empty_non_defaulted[0]) == &(non_default.object()[1]));
+        }
+
+        SECTION("Adding duplicate value does nothing") {
+            e0.insert(i0);
+            REQUIRE(e0.size() == 1);
+            REQUIRE(&(e0[0]) == &(non_default.object()[0]));
+        }
     }
 }
