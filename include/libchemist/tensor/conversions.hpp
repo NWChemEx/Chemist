ff55d6baa6f4118ac68dd0fbc582cd209f27cd2b
Ryan Richard <ryanmrichard1@gmail.com>
2021-09-15 16:50:23 -0500

    Starts a series of tensor conversions (#238)
    * starts a series of tensor conversions
    
    * dociumentation

---
+++ include/libchemist/tensor/conversions.hpp
@@ -0,0 +1,22 @@
+#pragma once
+#include "libchemist/tensor/types.hpp"
+
+namespace libchemist::tensor {
+
+/** @brief Flattens a tensor out into an std::vector.
+ *
+ *  This function will replicate the provided tensor (assuming it is distributed
+ *  to begin with) and then flatten it into a vector. The flattening will
+ *  occur in row-major format.
+ *
+ *  @note This function is primarily intended for use with low rank @p t, e.g.
+ *        in taking actions based on the value of an eigenvalue. Direct access
+ *        of high-rank tensors should be avoided.
+ *
+ *  @param[in] t The tensor to vectorize.
+ *
+ *  @return The tensor vectorized into an std::vector.
+ */
+std::vector<double> to_vector(const type::SparseTensorWrapper& t);
+
+} // namespace libchemist::tensor
