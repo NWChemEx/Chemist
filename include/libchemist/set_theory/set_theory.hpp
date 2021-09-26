a392167bed7c1c542b52d3dfea8c12873016d79f
Ryan Richard <ryanmrichard1@gmail.com>
2021-09-26 10:56:53 -0500

    remove ambiguous overload [skip ci]

--- include/libchemist/set_theory/set_theory.hpp
+++ include/libchemist/set_theory/set_theory.hpp
@@ -1,5 +1,5 @@
 #pragma once
 #include "libchemist/set_theory/family_of_sets.hpp"
-#include "libchemist/set_theory/set_traits.hpp"
 #include "libchemist/set_theory/subset.hpp"
+#include "libchemist/set_theory/traits/traits.hpp"
 #include "libchemist/set_theory/type_traits.hpp"
