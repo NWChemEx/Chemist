322fef7600c910b67664293e018393fa0c48d012
ryan <ryanmrichard1@gmail.com>
2021-09-27 15:28:46 -0500

    adds reshape

--- include/libchemist/tensor/tensor_wrapper.hpp
+++ include/libchemist/tensor/tensor_wrapper.hpp
@@ -205,10 +205,16 @@ public:
 
     /** @brief Used to view the tensor as if it has a different shape.
      *
-     *  This function is mainly used to flatten and unflatten a tensor.
+     *  This function is mainly used to flatten and un-flatten a tensor. Under
+     *  the hood this function remaps indices by:
+     *  - mapping the original coordinate index to its ordinal index
+     *  - mapping the ordinal index to the new coordinate index
+     *  The data is then redistributed, if needed.
      *
      *  @param[in] shape The shape of the resulting tensor. The volume of
      *                   @p shape must match the result of `size()`.
+     *
+     *  @return A copy of the current tensor with the new shape.
      *
      *  @throw std::runtime_error if @p shape does not have the same volume as
      *                            the wrapped tensor. Strong throw guarantee.
--- tests/tensor/tensor_wrapper.cpp
+++ tests/tensor/tensor_wrapper.cpp
@@ -159,6 +159,13 @@ TEMPLATE_LIST_TEST_CASE("TensorWrapper", "", type::tensor_variant) {
             auto rv = mat.reshape({4ul});
             REQUIRE(rv == corr);
         }
+        SECTION("tensor to matrix") {
+            TWrapper corr(
+              t_type(world, matrix_il{vector_il{1.0, 2.0, 3.0, 4.0},
+                                      vector_il{5.0, 6.0, 7.0, 8.0}}));
+            auto rv = t3.reshape({2ul, 4ul});
+            REQUIRE(rv == corr);
+        }
     }
 
     SECTION("operator()") {
