3311e9bfdea2c8f961ae6671e436d16760e541a1
ryan <ryanmrichard1@gmail.com>
2021-09-30 14:37:38 -0500

    adds make_pair_map [skip ci]

---
+++ tests/sparse_map/make_pair_map.cpp
@@ -0,0 +1,63 @@
+#include "test_sparse_map.hpp"
+
+using namespace libchemist::sparse_map;
+
+TEMPLATE_LIST_TEST_CASE("make_pair_map", "", testing::index_pairs_tuple_t) {
+    using ind_idx_type    = std::tuple_element_t<0, TestType>;
+    using dep_idx_type    = std::tuple_element_t<1, TestType>;
+    using ind_dep_sm_type = SparseMap<ind_idx_type, dep_idx_type>;
+    using ind_ind_sm_type = SparseMap<ind_idx_type, ind_idx_type>;
+
+    // Make indices we'll need
+    const auto& [i0, i1, i2, i3, i4] = testing::make_indices<ind_idx_type, 1>();
+    const auto& [j0, j1, j2, j3, j4] = testing::make_indices<dep_idx_type, 1>();
+    const auto& [i00, i01, i10, i11] = testing::make_indices<ind_idx_type, 2>();
+
+    ind_idx_type i22{i2[0], i2[0]}, i23{i2[0], i3[0]}, i32{i3[0], i2[0]};
+    ind_idx_type i33{i3[0], i3[0]};
+
+    SECTION("Normal usage") {
+        // Make sparse maps
+        ind_dep_sm_type L_ia{{i0, {j1}}, {i1, {j2}}, {i2, {j3}}, {i3, {j4}}};
+        ind_ind_sm_type L_ij{
+          {i0, {i0, i1}}, {i1, {i0, i1}}, {i2, {i2, i3}}, {i3, {i2, i3}}};
+
+        // Correct answer
+        ind_dep_sm_type corr{{i00, {j1}},     {i01, {j1, j2}}, {i10, {j1, j2}},
+                             {i11, {j2}},     {i22, {j3}},     {i23, {j3, j4}},
+                             {i32, {j3, j4}}, {i33, {j4}}};
+
+        auto rv = make_pair_map(L_ia, L_ij);
+        REQUIRE(rv == corr);
+    }
+
+    SECTION("Throws if i  in L_ij is not in L_ia") {
+        ind_dep_sm_type L_ia{{i1, {j1}}};
+        ind_ind_sm_type L_ij{{i0, {i1}}};
+        REQUIRE_THROWS_AS(make_pair_map(L_ia, L_ij), std::out_of_range);
+    }
+
+    SECTION("Throws if j in L_ij is not in L_ia") {
+        ind_dep_sm_type L_ia{{i1, {j1}}};
+        ind_ind_sm_type L_ij{{i1, {i0}}};
+        REQUIRE_THROWS_AS(make_pair_map(L_ia, L_ij), std::out_of_range);
+    }
+
+    SECTION("Throws if independent rank of L_ij != independent rank of L_ia") {
+        ind_dep_sm_type L_ia{{i1, {j1}}};
+        ind_ind_sm_type L_ij{{i01, {i1}}};
+        REQUIRE_THROWS_AS(make_pair_map(L_ia, L_ij), std::runtime_error);
+    }
+
+    SECTION("Throws if dependent rank of L_ij != independent rank of L_ia") {
+        ind_dep_sm_type L_ia{{i1, {j1}}};
+        ind_ind_sm_type L_ij{{i1, {i01}}};
+        REQUIRE_THROWS_AS(make_pair_map(L_ia, L_ij), std::runtime_error);
+    }
+
+    SECTION("Throws if independent rank of L_ia != 1") {
+        ind_dep_sm_type L_ia{{i01, {j1}}};
+        ind_ind_sm_type L_ij{{i01, {i01}}};
+        REQUIRE_THROWS_AS(make_pair_map(L_ia, L_ij), std::runtime_error);
+    }
+}
---
+++ tests/sparse_map/test_sparse_map.hpp
@@ -0,0 +1,31 @@
+#pragma once
+#include "libchemist/sparse_map/sparse_map.hpp"
+#include <catch2/catch.hpp>
+
+namespace testing {
+
+using index_type_tuple_t = std::tuple<libchemist::sparse_map::ElementIndex,
+                                      libchemist::sparse_map::TileIndex>;
+
+template<typename T, typename U>
+using index_pairs_tuple_t_ = std::tuple<std::pair<T, T>, std::pair<T, U>,
+                                        std::pair<U, T>, std::pair<U, U>>;
+
+using index_pairs_tuple_t =
+  index_pairs_tuple_t_<libchemist::sparse_map::ElementIndex,
+                       libchemist::sparse_map::TileIndex>;
+
+template<typename IndexType, std::size_t N>
+auto make_indices() {
+    if constexpr(N == 1) {
+        IndexType i0{0}, i1{1}, i2{2}, i3{3}, i4{4};
+        return std::make_tuple(i0, i1, i2, i3, i4);
+    } else if constexpr(N == 2) {
+        IndexType i00{0, 0}, i01{0, 1}, i10{1, 0}, i11{1, 1};
+        return std::make_tuple(i00, i01, i10, i11);
+    } else {
+        static_assert(N == 1, "Only support rank 1 and 2 indices");
+    }
+}
+
+} // namespace testing
