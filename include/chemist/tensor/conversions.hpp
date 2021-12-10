66ce25eaa0332c8f6126b16f0839cc1334aaa6e8
David Williams-Young <dbwy@lbl.gov>
2021-12-10 09:02:46 -0800

    Added TensorWrapper conversion to already exsiting contiguous buffer, to_vector now delegates to this implementation

--- include/chemist/tensor/conversions.hpp
+++ include/chemist/tensor/conversions.hpp
@@ -19,4 +19,20 @@ namespace chemist::tensor {
  */
 std::vector<double> to_vector(const ScalarTensorWrapper& t);
 
+/** @brief Flattens a tensor out into a contiguous buffer
+ *
+ *  This function will replicate the provided tensor (assuming it is distributed
+ *  to begin with) and then flatten it into a contiguous buffer. The flattening will
+ *  occur in row-major format.
+ *  
+ *  @note This function is primarily intended for use with low rank @p t, e.g.
+ *        in taking actions based on the value of an eigenvalue. Direct access
+ *        of high-rank tensors should be avoided.
+ *
+ *  @param[in]    t The tensor to vectorize.
+ *  @param[out]   buffer_begin The start of the contiguous buffer
+ *  @param[out]   buffer_end   The end of the contiguous buffer
+ */
+void to_contiguous_buffer( const ScalarTensorWrapper& t, double* buffer_begin, double* buffer_end );
+
 } // namespace chemist::tensor
--- src/chemist/tensor/conversions.cpp
+++ src/chemist/tensor/conversions.cpp
@@ -2,6 +2,7 @@
 
 namespace chemist::tensor {
 
+#if 0
 std::vector<double> to_vector(const ScalarTensorWrapper& t) {
     auto t_ta = t.get<TA::TSpArrayD>();
     t_ta.make_replicated();
@@ -13,5 +14,32 @@ std::vector<double> to_vector(const ScalarTensorWrapper& t) {
     }
     return rv;
 }
+#else
+void to_contiguous_buffer( const ScalarTensorWrapper& t, double* buffer_begin, double* buffer_end ) {
+
+    if( std::distance( buffer_begin, buffer_end ) != t.size() ) {
+        throw std::runtime_error("to_contiguous_buffer requires buffer size to be exactly the size of the tensor");
+    }
+
+    // XXX: In principle we don't have to make things replicated explictly, 
+    // we can just grab pieces remotely via RPC
+    auto t_ta = t.get<TA::TSpArrayD>();
+    t_ta.make_replicated();
+
+    // XXX: This prohibits vectorization, should be looked into
+    for(const auto& tile_i : t_ta) {
+        const auto& i_range = tile_i.get().range();
+        for(auto idx : i_range) buffer_begin[i_range.ordinal(idx)] = tile_i.get()[idx];
+    }
+}
+
+std::vector<double> to_vector(const ScalarTensorWrapper& t) {
+    std::vector<double> rv( t.size(), 0.0 );
+    to_contiguous_buffer( t, rv.data(), rv.data() + rv.size() );
+    return rv;
+}
+
+
+#endif
 
 } // namespace chemist::tensor
