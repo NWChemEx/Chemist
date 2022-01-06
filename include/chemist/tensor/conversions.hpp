fff3ccc24a0f9b2481757a6b88cfc72fdd8a87b3
Clang Robot <robot@example.com>
2022-01-06 18:03:14 +0000

    Committing clang-format changes

--- include/chemist/tensor/conversions.hpp
+++ include/chemist/tensor/conversions.hpp
@@ -22,9 +22,9 @@ std::vector<double> to_vector(const ScalarTensorWrapper& t);
 /** @brief Flattens a tensor out into a contiguous buffer
  *
  *  This function will replicate the provided tensor (assuming it is distributed
- *  to begin with) and then flatten it into a contiguous buffer. The flattening will
- *  occur in row-major format.
- *  
+ *  to begin with) and then flatten it into a contiguous buffer. The flattening
+ * will occur in row-major format.
+ *
  *  @note This function is primarily intended for use with low rank @p t, e.g.
  *        in taking actions based on the value of an eigenvalue. Direct access
  *        of high-rank tensors should be avoided.
@@ -33,6 +33,7 @@ std::vector<double> to_vector(const ScalarTensorWrapper& t);
  *  @param[out]   buffer_begin The start of the contiguous buffer
  *  @param[out]   buffer_end   The end of the contiguous buffer
  */
-void to_contiguous_buffer( const ScalarTensorWrapper& t, double* buffer_begin, const double* buffer_end );
+void to_contiguous_buffer(const ScalarTensorWrapper& t, double* buffer_begin,
+                          const double* buffer_end);
 
 } // namespace chemist::tensor
