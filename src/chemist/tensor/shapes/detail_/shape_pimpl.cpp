4d2ad82500a6030a1d987e69c03775a84976d962
Jonathan M. Waldrop <relik107@gmail.com>
2022-01-20 13:27:40 -0600

    remove files that live in TensorWrapper [skip ci]

--- src/chemist/tensor/shapes/detail_/shape_pimpl.cpp
+++
@@ -1,8 +0,0 @@
-#include "shape_pimpl.hpp"
-
-namespace chemist::tensor::detail_ {
-
-template class ShapePIMPL<field::Scalar>;
-template class ShapePIMPL<field::Tensor>;
-
-} // namespace chemist::tensor::detail_
