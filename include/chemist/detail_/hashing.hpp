3972fbb0c9ecbff14a1f5df6ce78d98a316fafa4
Jonathan M. Waldrop <relik107@gmail.com>
2022-01-26 14:56:38 -0600

    update parallelzone namespace

--- include/chemist/detail_/hashing.hpp
+++ include/chemist/detail_/hashing.hpp
@@ -5,12 +5,12 @@
 namespace chemist::detail_ {
 
 // From ParallelZone
-using pz::hash_objects;
-using pz::hash_to_string;
-using pz::Hasher;
-using pz::HashType;
-using pz::HashValue;
-using pz::make_hash;
-using pz::make_hasher;
+using parallelzone::hash_objects;
+using parallelzone::hash_to_string;
+using parallelzone::Hasher;
+using parallelzone::HashType;
+using parallelzone::HashValue;
+using parallelzone::make_hash;
+using parallelzone::make_hasher;
 
 } // namespace chemist::detail_
