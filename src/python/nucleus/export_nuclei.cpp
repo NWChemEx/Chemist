5e08e12ad3bc23d48e1c46292a13548cb0c97614
Clang Robot <robot@example.com>
2023-05-23 21:02:47 +0000

    Committing clang-format changes

--- src/python/nucleus/export_nuclei.cpp
+++ src/python/nucleus/export_nuclei.cpp
@@ -21,14 +21,14 @@
 namespace chemist {
 
 void export_nuclei(python_module_reference m) {
-        // N.B. We don't want to export the base class so we use lambdas to
+    // N.B. We don't want to export the base class so we use lambdas to
     //      call the methods.
 
     using nuclei_type      = Nuclei;
     using nuclei_reference = nuclei_type&;
-    using value_type        = typename nuclei_type::value_type;
-    using reference         = typename nuclei_type::reference;
-    using size_type         = typename nuclei_type::size_type;
+    using value_type       = typename nuclei_type::value_type;
+    using reference        = typename nuclei_type::reference;
+    using size_type        = typename nuclei_type::size_type;
 
     using at_fxn = reference (nuclei_type::*)(size_type);
 
