f5a517705fe9cb5958be676d789019cccb4e34a5
ryan <ryanmrichard1@gmail.com>
2023-06-07 10:40:02 -0500

    cap class done [skip ci]

--- include/chemist/capping/cap.hpp
+++ include/chemist/capping/cap.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include <chemist/molecule/molecule.hpp>
+#include <optional>
 
 namespace chemist {
 
@@ -27,46 +28,170 @@ public:
     /// Type used by *this for indexing and offsets. Unsigned integral type.
     using size_type = Molecule::size_type;
 
-    /// Type of a read/write reference to an object of type size_type
-    using size_reference = size_type&;
-
-    /// Type of a read-only reference to an object of type size_type
-    using const_size_reference = const size_type&;
-
     /** @brief Creates a defaulted cap.
      *
-     *  A defaulted cap is comprised of zero atoms, is anchored on atom 0, and
-     *  replaces atom 0 (the latter two conditions are simply the default
-     *  state)
+     *  A defaulted cap is comprised of zero atoms, has no anchor atom, and
+     *  does not replace any atom.
+     *
+     *  @throw None No throw guarantee.
      */
     Cap() = default;
 
+    /** @brief Creates a cap with the provided state.
+     *
+     *  @tparam Args The types of the atoms comprising the cap. Each type in the
+     *               parameter pack is assumed to be implicitly convertible to
+     *               an object of type atom_type.
+     *
+     *  @param[in] anchor The index of the atom to which *this is anchored. It
+     *                    is the responsibility of the user to ensure that the
+     *                    index is correct and in bounds for their molecular
+     *                    system.
+     *  @param[in] replaced The index of the atom being replaced by *this. It
+     *                      is the responsibility of the user to ensure that
+     *                      the index is correct and in bounds for their
+     *                      molecular system.
+     *
+     *  @param[in] atoms The literal atoms comprising the cap.
+     *
+     *  @throw std::bad_alloc if there is a problem allocating state for *this.
+     *                        Strong throw guarantee.
+     */
     template<typename... Args>
     Cap(size_type anchor, size_type replaced, Args&&... atoms) :
       m_atoms_{std::forward<Args>(atoms)...},
       m_anchor_(anchor),
       m_replaced_(replaced) {}
 
+    /** @brief Allows adding additional atoms to the cap.
+     *
+     *  @param[in] atom The atom to add to the cap represented by *this.
+     *
+     *  @throw std::bad_alloc if there is a problem allocating memory for the
+     *                        new atom. Strong throw guarantee.
+     */
     void add_cap_atom(atom_type atom) {
         m_atoms_.emplace_back(std::move(atom));
     }
 
-    atom_reference atom(size_type i) { return m_atoms_.at(i); }
+    /** @brief Returns the @p i -th atom comprising *this.
+     *
+     *  This method is used to access the atoms which have been added to *this.
+     *  The atoms are stored in the order they were added. So atom 0 will be the
+     *  first atom added, atom 1 the second, etc.
+     *
+     *  @param[in] i The index of the atom to retrieve. @p i must be in the
+     *               range [0, size()).
+     *
+     *  @return A mutable reference to the @p i -th atom.
+     *
+     *  @throw std::out_of_range if @p i is not in the range [0, size()).
+     *                           Strong throw guarantee.
+     */
+    atom_reference cap_atom(size_type i) { return m_atoms_.at(i); }
 
-    const_atom_reference atom(size_type i) const { return m_atoms_.at(i); }
+    /** @brief Returns the @p i-th atom comprising *this.
+     *
+     *  This method is the same as the non-const version except that the
+     *  returned atom is read-only.
+     *
+     *  @param[in] i The index of the atom to retrieve. @p i must be in the
+     *             range [0, size()).
+     *
+     *  @return A read-only reference to the @p i -th atom.
+     *
+     *  @throw std::out_of_range if @p i is not in the range [0, size()).
+     *                           Strong throw guarantee.
+     */
+    const_atom_reference cap_atom(size_type i) const { return m_atoms_.at(i); }
 
+    /** @brief How many atoms comprise *this?
+     *
+     *  @return The number of atoms comprising *this.
+     *
+     *  @throw None No throw guarantee.
+     */
     size_type size() const noexcept { return m_atoms_.size(); }
 
-    size_reference anchor_index() noexcept { return m_anchor_; }
+    /** @brief Sets the index of the anchor atom.
+     *
+     *  This method can be used to set the index of the atom to which *this is
+     *  attached. If the anchor index was already set, this function will
+     *  overwrite the value.
+     *
+     *  @param[in] anchor The index of the atom to which *this is anchored.
+     *
+     *  @throw None No throw guarantee.
+     */
+    void set_anchor_index(size_type anchor) noexcept {
+        m_anchor_.emplace(anchor);
+    }
 
-    const_size_reference anchor_index() const noexcept { return m_anchor_; }
+    /** @brief Gets the index of the anchor atom.
+     *
+     *  Assuming the user has set the index of the anchor atom, this method
+     *  can be used to get that index.
+     *
+     *  @return The index of the atom *this is attached to.
+     *
+     *  @throw std::bad_optional_access if the anchor atom has not been set.
+     *                                  Strong throw guarantee.
+     */
+    size_type get_anchor_index() const { return m_anchor_.value(); }
 
-    size_reference replaced_index() noexcept { return m_replaced_; }
+    /** @brief Sets the index of the atom *this replaces.
+     *
+     *  This method can be used to set the index of the atom being replaced by
+     *  *this.
+     *
+     *  @param[in] replaced The index of the atom *this replaces.
+     *
+     *  @throw None No throw guarantee.
+     */
+    void set_replaced_index(size_type replaced) noexcept {
+        m_replaced_.emplace(replaced);
+    }
 
-    const_size_reference replaced_index() const noexcept { return m_replaced_; }
+    /** @brief What is the index of the atom replaced by *this?
+     *
+     *  This method can be used to retrieve the index of the atom which *this
+     *  replaces (assuming it has been set).
+     *
+     *  @return The index of the atom *this replaces.
+     *
+     *  @throw std::bad_optional_access if the index has not been set. Strong
+     *                                  throw guarantee.
+     */
+    size_type get_replaced_index() const { return m_replaced_.value(); }
 
+    /** @brief Determines if two cap instances are value equal.
+     *
+     *  Two non-default Cap instances are value equal if they both are
+     *  anchored to the same atom, replace the same atom, and are comprised of
+     *  atoms which are value equal (atoms must also be stored in the same
+     *  order). Cap instances may also be value equal if they are both default
+     *  instances.
+     *
+     *  @param[in] rhs The instance we are comparing *this to.
+     *
+     *  @return True if *this is value equal to @p rhs and false otherwise.
+     *
+     *  @throw None No throw guarantee.
+     */
     bool operator==(const Cap& rhs) const noexcept;
 
+    /** @brief Determines if two Cap instances are different.
+     *
+     *  Cap instances are different if they are not value equal. This function
+     *  simply negates operator==. See the description of operator== for the
+     *  definition of value equal.
+     *
+     *  @param[in] rhs The instance we are comparing *this to.
+     *
+     *  @return False if *this is value equal @p rhs and true otherwise.
+     *
+     *  @throw None No throw guarantee.
+     */
     bool operator!=(const Cap& rhs) const noexcept { return !(*this == rhs); }
 
 private:
@@ -74,14 +199,18 @@ private:
     auto state_() const { return std::tie(m_anchor_, m_replaced_, m_atoms_); }
 
     /// The index (in the parent molecule) of the atom the cap is attached to
-    size_type m_anchor_ = 0;
+    std::optional<size_type> m_anchor_;
 
     /// The index (in the parent molecule) of the atom replaced by this cap
-    size_type m_replaced_ = 0;
+    std::optional<size_type> m_replaced_;
 
     /// The atom(s) actually comprising the cap
     atom_vector m_atoms_;
 };
+
+// -----------------------------------------------------------------------------
+// -- Out of line inline definitions
+// -----------------------------------------------------------------------------
 
 inline bool Cap::operator==(const Cap& rhs) const noexcept {
     return state_() == rhs.state_();
--- tests/cxx/unit_tests/capping/cap.cpp
+++ tests/cxx/unit_tests/capping/cap.cpp
@@ -16,23 +16,20 @@ TEST_CASE("Cap") {
     Cap c23(2, 3, atom0, atom1);
 
     SECTION("CTors") {
-        SECTION("default") {
-            REQUIRE(defaulted.size() == 0);
-            REQUIRE(defaulted.anchor_index() == 0);
-            REQUIRE(defaulted.replaced_index() == 0);
-        }
+        SECTION("default") { REQUIRE(defaulted.size() == 0); }
         SECTION("Value") {
             REQUIRE(c12.size() == 1);
-            REQUIRE(c12.anchor_index() == 1);
-            REQUIRE(c12.replaced_index() == 2);
-            REQUIRE(c12.atom(0) == atom0);
+            REQUIRE(c12.get_anchor_index() == 1);
+            REQUIRE(c12.get_replaced_index() == 2);
+            REQUIRE(c12.cap_atom(0) == atom0);
 
             REQUIRE(c23.size() == 2);
-            REQUIRE(c23.anchor_index() == 2);
-            REQUIRE(c23.replaced_index() == 3);
-            REQUIRE(c23.atom(0) == atom0);
-            REQUIRE(c23.atom(1) == atom1);
+            REQUIRE(c23.get_anchor_index() == 2);
+            REQUIRE(c23.get_replaced_index() == 3);
+            REQUIRE(c23.cap_atom(0) == atom0);
+            REQUIRE(c23.cap_atom(1) == atom1);
         }
+
         SECTION("Copy") {
             Cap other_defaulted(defaulted);
             REQUIRE(defaulted == other_defaulted);
@@ -43,6 +40,7 @@ TEST_CASE("Cap") {
             Cap other_c23(c23);
             REQUIRE(c23 == other_c23);
         }
+
         SECTION("Moved") {
             Cap other_defaulted(defaulted);
             Cap moved_defaulted(std::move(defaulted));
@@ -56,6 +54,7 @@ TEST_CASE("Cap") {
             Cap moved_c23(std::move(c23));
             REQUIRE(moved_c23 == other_c23);
         }
+
         SECTION("Copy Assignment") {
             Cap other_defaulted;
             auto pother_defaulted = &(other_defaulted = defaulted);
@@ -72,7 +71,8 @@ TEST_CASE("Cap") {
             REQUIRE(c23 == other_c23);
             REQUIRE(pother_c23 == &other_c23);
         }
-        SECTION("Moved") {
+
+        SECTION("Move Assignment") {
             Cap other_defaulted(defaulted);
             Cap moved_defaulted;
             auto pmoved_defaulted = &(moved_defaulted = std::move(defaulted));
@@ -91,5 +91,116 @@ TEST_CASE("Cap") {
             REQUIRE(moved_c23 == other_c23);
             REQUIRE(pmoved_c23 == &moved_c23);
         }
+    }
+
+    SECTION("add_cap_atom") {
+        Cap other_c23(2, 3, atom0);
+        REQUIRE(other_c23 != c23);
+
+        other_c23.add_cap_atom(atom1);
+        REQUIRE(other_c23 == c23);
+    }
+
+    SECTION("cap_atom") {
+        REQUIRE_THROWS_AS(defaulted.cap_atom(0), std::out_of_range);
+
+        REQUIRE(c12.cap_atom(0) == atom0);
+        REQUIRE_THROWS_AS(c12.cap_atom(1), std::out_of_range);
+
+        REQUIRE(c23.cap_atom(0) == atom0);
+        REQUIRE(c23.cap_atom(1) == atom1);
+        REQUIRE_THROWS_AS(c23.cap_atom(2), std::out_of_range);
+
+        // Is writeable?
+        c12.cap_atom(0) = atom1;
+        REQUIRE(c12.cap_atom(0) == atom1);
+    }
+
+    SECTION("cap_atom() const") {
+        REQUIRE_THROWS_AS(std::as_const(defaulted).cap_atom(0),
+                          std::out_of_range);
+
+        REQUIRE(std::as_const(c12).cap_atom(0) == atom0);
+        REQUIRE_THROWS_AS(std::as_const(c12).cap_atom(1), std::out_of_range);
+
+        REQUIRE(std::as_const(c23).cap_atom(0) == atom0);
+        REQUIRE(std::as_const(c23).cap_atom(1) == atom1);
+        REQUIRE_THROWS_AS(std::as_const(c23).cap_atom(2), std::out_of_range);
+    }
+
+    SECTION("size") {
+        REQUIRE(defaulted.size() == 0);
+        REQUIRE(c12.size() == 1);
+        REQUIRE(c23.size() == 2);
+    }
+
+    SECTION("set_anchor_index") {
+        defaulted.set_anchor_index(1);
+        REQUIRE(defaulted.get_anchor_index() == 1);
+
+        // Can overwrite existing value
+        c12.set_anchor_index(3);
+        REQUIRE(c12.get_anchor_index() == 3);
+    }
+
+    SECTION("get_anchor_index") {
+        REQUIRE_THROWS_AS(defaulted.get_anchor_index(),
+                          std::bad_optional_access);
+
+        REQUIRE(c12.get_anchor_index() == 1);
+        REQUIRE(c23.get_anchor_index() == 2);
+    }
+
+    SECTION("set_replaced_index") {
+        defaulted.set_replaced_index(1);
+        REQUIRE(defaulted.get_replaced_index() == 1);
+
+        // Can overwrite existing value
+        c12.set_replaced_index(3);
+        REQUIRE(c12.get_replaced_index() == 3);
+    }
+
+    SECTION("get_replaced_index") {
+        REQUIRE_THROWS_AS(defaulted.get_replaced_index(),
+                          std::bad_optional_access);
+
+        REQUIRE(c12.get_replaced_index() == 2);
+        REQUIRE(c23.get_replaced_index() == 3);
+    }
+
+    SECTION("comparisons") {
+        // Default v default
+        Cap other_default;
+        REQUIRE(other_default == defaulted);
+        REQUIRE_FALSE(other_default != defaulted);
+
+        // Default vs non-default
+        REQUIRE(defaulted != c12);
+        REQUIRE_FALSE(defaulted == c12);
+
+        // Same value
+        Cap other_c12(1, 2, atom0);
+        REQUIRE(c12 == other_c12);
+        REQUIRE_FALSE(c12 != other_c12);
+
+        // Different anchor
+        Cap c42(4, 2, atom0);
+        REQUIRE(c42 != c12);
+        REQUIRE_FALSE(c42 == c12);
+
+        // Different replaced
+        Cap c14(1, 4, atom0);
+        REQUIRE(c14 != c12);
+        REQUIRE_FALSE(c14 == c12);
+
+        // Different atom
+        other_c12.cap_atom(0) = atom1;
+        REQUIRE(c12 != other_c12);
+        REQUIRE_FALSE(c12 == other_c12);
+
+        // Different atom order
+        Cap other_c23(2, 3, atom1, atom0);
+        REQUIRE(c23 != other_c23);
+        REQUIRE_FALSE(c23 == other_c23);
     }
 }
