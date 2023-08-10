71e5fecc0aabfbfa94af870283034b514665d887
Ryan M. Richard <ryanmrichard1@gmail.com>
2023-08-09 22:34:32 -0500

    backup [skip ci]

--- include/chemist/nucleus/nuclei_view.hpp
+++ include/chemist/nucleus/nuclei_view.hpp
@@ -23,6 +23,9 @@ public:
     /// Type of a pointer to the PIMPL
     using pimpl_pointer = std::unique_ptr<pimpl_type>;
 
+    /// Type used for indexing and offsets
+    using size_type = typename base_type::size_type;
+
     // -- Nuclei/Nucleus types -------------------------------------------------
 
     /// Type *this is behaving like
@@ -42,9 +45,30 @@ public:
     /// Type used to store the charge
     using charge_type = typename nuclei_type::charge_type;
 
-    
+    // -- Ctors, Assignment, and dtor ------------------------------------------
 
+    NucleiView(pimpl_pointer pimpl) noexcept;
 
+    /// Default no throw dtor
+    ~NucleiView() noexcept;
+private:
+    /// Allows the base class to access at_ and size_
+    friend base_type;
+
+    /// Implements retrieving read/write references
+    reference at_(size_type i);
+
+    /// Implements retrieving read-only references
+    const_reference at_(size_type i) const;
+
+    /// Used by base to determine the number of elements in *this
+    size_type size_() const noexcept;
+
+    /// Wraps determining if *this has a PIMPL or not
+    bool has_pimpl_() const noexcept;
+
+    /// The object implementing *this
+    pimpl_pointer m_pimpl_;
 
 };
 
---
+++ src/chemist/nucleus/nuclei_view/nuclei_view.cpp
@@ -0,0 +1,35 @@
+#include "detail_/nuclei_view_pimpl.hpp"
+
+namespace chemist {
+
+// -----------------------------------------------------------------------------
+// -- CTors, Assignment, and Dtor
+// -----------------------------------------------------------------------------
+
+NucleiView::NucleiView(pimpl_pointer pimpl) noexcept :
+    m_pimpl_(std::move(pimpl)) {}
+
+NucleiView::~NucleiView() noexcept = default;
+
+// -----------------------------------------------------------------------------
+// -- Protected and Private members
+// -----------------------------------------------------------------------------
+
+typename NucleiView::reference NucleiView::at_(size_type i) {
+    return m_pimpl_->get_nuke(i);
+}
+
+typename NucleiView::const_reference NucleiView::at_(size_type i) const {
+    return std::as_const(*m_pimpl_).get_nuke(i);
+}
+
+typename NucleiView::size_type NucleiView::size_() const noexcept {
+    if(has_pimpl_()) return m_pimpl_->size();
+    return 0;
+}
+
+bool NucleiView::has_pimpl_() const noexcept {
+    return static_cast<bool>(m_pimpl_);
+}
+
+}
