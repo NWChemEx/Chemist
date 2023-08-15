4c1165aa1f162669d10f502e8036b30852e54803
Jonathan M. Waldrop <relik107@gmail.com>
2023-08-15 15:02:52 -0500

    primitive exposed

--- src/python/basis_set/export_primitive.cpp
+++ src/python/basis_set/export_primitive.cpp
@@ -15,15 +15,43 @@
  */
 
 #include "export_basis_set.hpp"
- 
+#include <chemist/basis_set2/primitive/primitive.hpp>
+
 namespace chemist {
 namespace detail_ {
 
 template<typename T>
-void export_primitive_(python_module_reference m) {}
+void export_primitive_(const char* name, python_module_reference m) {
+    using primitive_type = basis_set::Primitive<T>;
+    using coeff_type     = typename primitive_type::coefficient_type;
+    using expo_type      = typename primitive_type::exponent_type;
+    using center_type    = typename primitive_type::center_type;
+    using coord_type     = typename primitive_type::coord_type;
+
+    python_class_type<primitive_type>(m, name)
+      .def(pybind11::init<>())
+      .def(pybind11::init<coeff_type, expo_type, coord_type, coord_type,
+                          coord_type>())
+      .def(pybind11::init<coeff_type, expo_type, center_type>())
+      .def_property(
+        "center", [](primitive_type& p) { return p.center(); },
+        [](primitive_type& p, center_type c) { p.center() = c; })
+      .def_property(
+        "coefficient", [](primitive_type& p) { return p.coefficient(); },
+        [](primitive_type& p, coeff_type c) { p.coefficient() = c; })
+      .def_property(
+        "exponent", [](primitive_type& p) { return p.exponent(); },
+        [](primitive_type& p, expo_type e) { p.exponent() = e; })
+      .def("is_null", [](primitive_type& p) { return p.is_null(); })
+      .def(pybind11::self == pybind11::self)
+      .def(pybind11::self != pybind11::self);
+}
 
 } // namespace detail_
 
-void export_primitive(python_module_reference m) {}
+void export_primitive(python_module_reference m) {
+    detail_::export_primitive_<float>("PrimitiveF", m);
+    detail_::export_primitive_<double>("PrimitiveD", m);
+}
 
 } // namespace chemist
