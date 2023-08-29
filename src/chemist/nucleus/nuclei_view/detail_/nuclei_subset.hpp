6fe6a5de927b421dbf316314aacc06f8cac2633f
Clang Robot <robot@example.com>
2023-08-29 14:44:09 +0000

    Committing clang-format changes

--- src/chemist/nucleus/nuclei_view/detail_/nuclei_subset.hpp
+++ src/chemist/nucleus/nuclei_view/detail_/nuclei_subset.hpp
@@ -86,9 +86,9 @@ public:
 
     // Deleted to avoid accidental slicing
     ///@{
-    NucleiSubset(NucleiSubset&&)                     = delete;
+    NucleiSubset(NucleiSubset&&) = delete;
     NucleiSubset& operator=(const NucleiSubset& rhs) = delete;
-    NucleiSubset& operator=(NucleiSubset&& rhs)      = delete;
+    NucleiSubset& operator=(NucleiSubset&& rhs) = delete;
     ///@}
 
     /** @brief Determines if *this is value equal to @p rhs.
