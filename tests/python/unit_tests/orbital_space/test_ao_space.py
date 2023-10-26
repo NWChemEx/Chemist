1f56565c92ead9a79cf74d115c0dc1daa78a236c
Murat Keceli <keceli@gmail.com>
2023-10-26 13:12:24 -0500

    Add python unit tests

--- tests/python/unit_tests/orbital_space/test_ao_space.py
+++ tests/python/unit_tests/orbital_space/test_ao_space.py
@@ -2,19 +2,36 @@ import unittest
 import chemist
 
 class TestAOSpace(unittest.TestCase):
-    def test_aospace(self):
+    def test_ao_space(self):
         # Test initialization
-        ao_space = chemist.AOSpaceD()
-        self.assertIsNotNone(ao_space)
+        ao_space_d = chemist.AOSpaceD()
+        ao_space_f = chemist.AOSpaceF()
+        self.assertIsNotNone(ao_space_d)
+        self.assertIsNotNone(ao_space_f)
+
+        # Test basis set
+        basis_set_d = chemist.basis_set.AOBasisSetD()
+        basis_set_f = chemist.basis_set.AOBasisSetF()
+        ao_space_d = chemist.AOSpaceD(basis_set_d)
+        ao_space_f = chemist.AOSpaceF(basis_set_f)
+        self.assertEqual(ao_space_d.basis_set, basis_set_d)
+        self.assertEqual(ao_space_f.basis_set, basis_set_f)
 
         # Test size
-        size = ao_space.size()
-        self.assertEqual(size, 0)
+        size_d = ao_space_d.size()
+        size_f = ao_space_f.size()
+        self.assertEqual(size_d, 0)
+        self.assertEqual(size_f, 0)
 
         # Test equality
-        ao_space2 = chemist.AOSpaceD()
-        self.assertEqual(ao_space, ao_space2)
+        ao_space_d2 = chemist.AOSpaceD()
+        ao_space_f2 = chemist.AOSpaceF()
+        self.assertEqual(ao_space_d, ao_space_d2)
+        self.assertEqual(ao_space_f, ao_space_f2)
 
         # Test inequality
-        ao_space3 = chemist.AOSpaceF()
-        self.assertNotEqual(ao_space, ao_space3)
+        center = chemist.basis_set.AtomicBasisSetD("", 0, 1.0, 2.0, 3.0)
+        basis_set_d.add_center(center)
+        ao_space_d2.basis_set = basis_set_d
+        self.assertEqual(ao_space_d2.size(), 1)
+        self.assertNotEqual(ao_space_d, ao_space_d2)
