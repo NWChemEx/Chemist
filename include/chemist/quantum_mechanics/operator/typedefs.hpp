35a0fc926831078b30066a79fc34f2f2cc536709
Ryan M. Richard <rrichard@ameslab.gov>
2024-12-16 10:03:52 -0600

    add typedef for Identity

--- include/chemist/quantum_mechanics/operator/typedefs.hpp
+++ include/chemist/quantum_mechanics/operator/typedefs.hpp
@@ -45,6 +45,7 @@ namespace chemist::qm_operator::types {
 
 using op_base_type = OperatorBase;
 
+using s_e_type = Identity;
 using t_e_type = Kinetic<Electron>;
 using T_e_type = Kinetic<ManyElectrons>;
 using t_n_type = Kinetic<Nucleus>;
