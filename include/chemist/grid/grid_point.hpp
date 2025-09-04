edbd897f44aabcff34214c7e31ee3e2640528e89
Ryan M. Richard <rrichard@ameslab.gov>
2025-09-04 15:39:38 -0500

    documentation tweak

--- include/chemist/grid/grid_point.hpp
+++ include/chemist/grid/grid_point.hpp
@@ -1,10 +1,26 @@
+/*
+ * Copyright 2025 NWChemEx-Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 #pragma once
 #include <chemist/point/point_class.hpp>
 #include <chemist/traits/grid_traits.hpp>
 
 namespace chemist {
 
-/** @brief Class describing a point on a grid.
+/** @brief Class describing a weighted grid point.
  *
  *  The GridPoint class models a single grid point. Each grid point within the
  *  grid is assumed to have:
