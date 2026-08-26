ad4dca3c6985741febe5665a10d13dd1476876d7
ryanmrichard <ryanmrichard@users.noreply.github.com>
2026-08-26 17:04:27 +0000

    deploy: a979ca25811743f59ba8bbfde25a130a28f9ff66

--- chemist_cxx_api/search/classes_10.js
+++ chemist_cxx_api/search/classes_10.js
@@ -3,5 +3,7 @@ var searchData=
   ['tensorelement_0',['TensorElement',['../classchemist_1_1braket_1_1TensorElement.html',1,'chemist::braket']]],
   ['tensorrepresentation_1',['TensorRepresentation',['../classchemist_1_1braket_1_1TensorRepresentation.html',1,'chemist::braket']]],
   ['transformed_2',['Transformed',['../classchemist_1_1wavefunction_1_1Transformed.html',1,'chemist::wavefunction']]],
-  ['transformed_3c_20aos_20_3e_3',['Transformed&lt; AOs &gt;',['../classchemist_1_1wavefunction_1_1Transformed.html',1,'chemist::wavefunction']]]
+  ['transformed_3c_20aos_20_3e_3',['Transformed&lt; AOs &gt;',['../classchemist_1_1wavefunction_1_1Transformed.html',1,'chemist::wavefunction']]],
+  ['type_5fpair_4',['type_pair',['../structchemist_1_1qm__operator_1_1detail___1_1type__pair.html',1,'chemist::qm_operator::detail_']]],
+  ['type_5ftag_5',['type_tag',['../structchemist_1_1qm__operator_1_1detail___1_1type__tag.html',1,'chemist::qm_operator::detail_']]]
 ];
--- chemist_cxx_api/search/classes_3.js
+++ chemist_cxx_api/search/classes_3.js
@@ -1,7 +1,8 @@
 var searchData=
 [
   ['decomposabledensity_0',['DecomposableDensity',['../classchemist_1_1DecomposableDensity.html',1,'chemist']]],
-  ['density_1',['density',['../classchemist_1_1Density.html',1,'chemist::Density&lt; Particles &gt;'],['../classchemist_1_1qm__operator_1_1Density.html',1,'chemist::qm_operator::Density&lt; T, Particles &gt;']]],
-  ['density_3c_20particles_2e_2e_2e_20_3e_2',['Density&lt; Particles... &gt;',['../classchemist_1_1Density.html',1,'chemist']]],
-  ['determinant_3',['Determinant',['../classchemist_1_1wavefunction_1_1Determinant.html',1,'chemist::wavefunction']]]
+  ['default_5fcustomize_1',['default_customize',['../structchemist_1_1qm__operator_1_1detail___1_1default__customize.html',1,'chemist::qm_operator::detail_']]],
+  ['density_2',['density',['../classchemist_1_1Density.html',1,'chemist::Density&lt; Particles &gt;'],['../classchemist_1_1qm__operator_1_1Density.html',1,'chemist::qm_operator::Density&lt; T, Particles &gt;']]],
+  ['density_3c_20particles_2e_2e_2e_20_3e_3',['Density&lt; Particles... &gt;',['../classchemist_1_1Density.html',1,'chemist']]],
+  ['determinant_4',['Determinant',['../classchemist_1_1wavefunction_1_1Determinant.html',1,'chemist::wavefunction']]]
 ];
